3572530f821dfa454366a7f3646f8e3211079aff
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2020-12-23 07:57:14 +0800

    password change for cypress test

--- cypress/integration/LoginForm.spec.js
+++ cypress/integration/LoginForm.spec.js
@@ -15,8 +15,8 @@ context('LoginForm', () => {
   it('Login', () => {
     cy.server({ method: 'POST' })
     cy.route('/*/*/*/auth**').as('postLogin')
-    cy.get('input[name=email]').type('sergio+admin@ordering.co')
-    cy.get('input[name=password]').type('test2020')
+    cy.get('input[name=email]').type('superadmin@ordering.co')
+    cy.get('input[name=password]').type('super')
     cy.get('button').contains('Login').click()
     cy.wait('@postLogin').its('status').should('eq', 200)
   })
