dac402f080ffa66fbd4a12039990a059cde1144a
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-03-20 12:25:33 +0800

    Remove unnecessary code

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -42,7 +42,6 @@ const LoginFormUI = (props) => {
   } = props
   const [, t] = useLanguage()
   const [ordering] = useApi()
-  // const [{ auth, user }] = useSession()
   const { handleSubmit, register, errors } = useForm()
   const [alertState, setAlertState] = useState({ open: false, content: [] })
   const [submitted, setSubmitted] = useState(false)
@@ -93,17 +92,6 @@ const LoginFormUI = (props) => {
       content: []
     })
   }
-
-  // useEffect(() => {
-  //   if (!auth || formState.loading) return
-  //   if (user?.level !== 0) {
-  //     setAlertState({
-  //       open: true,
-  //       content: t('YOU_DONT_PERMISSION', 'You don\'t have permission')
-  //     })
-  //   }
-  // }, [auth, formState.loading])
-
   return (
     <LoginContainer isPopup={isPopup}>
       <LoginHeroContainer bgimage={theme.images?.general?.loginHero}>
