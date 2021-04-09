07fd385d0777a01a158dde5b7e22472aa83dad94
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-04-09 19:44:35 +0800

    update Apr 9

--- src/components/MetaFields/styles.js
+++ src/components/MetaFields/styles.js
@@ -8,40 +8,86 @@ export const MetaTitle = styled.p`
   font-size: 18px;
   font-weight: 600;  
 `
-export const MetaTable = styled.table`
-  width: 100%;
+export const MetaContainer = styled.div`
+  display: flex;
+  margin-bottom: 20px;
 
-  tr {
-    td {
-      vertical-align: baseline;
-      padding: 5px 0;
-      
-      &:nth-child(3) {
-        word-break: break-all;
-      }
-      svg {
-        color: darkRed;
-        cursor: pointer;
-      }
+  .meta_type {
+    width: 20%;
+  }
+  .meta_key {
+    width: 25%;
+    padding: 0 10px;
+    box-sizing: border-box;
+  }
+
+  .meta_value {
+    flex: 1;
+    word-break: break-all;
+    display: flex;
+    column-gap: 10px;
+    > div {
+      width: 100%;
+    }
+    svg {
+      font-size: 24px;
+      color: darkRed;
+      cursor: pointer;
     }
   }
 
-  @media (min-width: 576px) {
-    tr {
-      td {
-        &:first-child {
-          min-width: 100px;
-        }
-      }
-    }
+`
+export const MetaAddContainer = styled(MetaContainer)`
+  input {
+    border: 1px solid #c2c2c2;
+    border-radius: 20px;
+    padding: 5px 15px;
+    outline: none;
+    font-size: 16px;
+    box-sizing: border-box;
+    width: 100%;
   }
 `
 export const RoundBorder = styled.div`
   border: 1px solid #c2c2c2;
   border-radius: 20px;
   padding: 5px 15px;
-  margin: 0 15px;
 `
 export const NoMetaField = styled.p`
   font-size: 14px;
+`
+export const MetaAddForm = styled.form`
+  button[type='submit'] {
+    outline: none;
+    background: transparent;
+    border: none;
+    padding: 0px;
+    height: fit-content
+  }
+
+  .select-input {
+    width: 100%;
+
+    svg {
+      color: #000;
+    }
+  }
+  
+  .json_editor {
+    width: 100%;
+    height: 300px;
+    > div {
+      height: 100%;
+    }
+    .jsoneditor {
+      border: 1px solid #182964;
+      > div.jsoneditor-menu {
+        background-color: #182964;
+        border-bottom: 1px solid #182964;
+      }
+    }
+    .jsoneditor-search {
+      display: none;
+    }
+  }
 `
