d881d287ff7289abd330ca7686413e95a19ad975
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-06-16 23:20:50 +0800

    feat businessActiveStateFilter

---
+++ src/themes/two/src/components/BusinessActiveStateFilter/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useLanguage } from 'ordering-components-admin'
+import { AutoScroll } from '../AutoScroll'
+import {
+  ActiveStateFilterContainer,
+  InnerContainer,
+  Tab
+} from './styles'
+
+export const BusinessActiveStateFilter = (props) => {
+  const {
+    selectedBusinessActiveState,
+    handleChangeBusinessActiveState
+  } = props
+
+  const [, t] = useLanguage()
+
+  return (
+    <>
+      <ActiveStateFilterContainer>
+        <InnerContainer>
+          <AutoScroll innerScroll scrollId='userActiveState'>
+            <Tab
+              active={selectedUserActiveState}
+              onClick={() => handleChangeBusinessActiveState()}
+            >
+              {t('ACTIVE', 'Active')}
+            </Tab>
+            <Tab
+              active={!selectedUserActiveState}
+              onClick={() => handleChangeBusinessActiveState()}
+            >
+              {t('INACTIVE', 'Inactive')}
+            </Tab>
+          </AutoScroll>
+        </InnerContainer>
+      </ActiveStateFilterContainer>
+    </>
+  )
+}
