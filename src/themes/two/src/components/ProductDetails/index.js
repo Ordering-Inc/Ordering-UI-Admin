04609bd3b48265d457396261246383e8c7dec617
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-07-06 02:53:47 +0800

    update

---
+++ src/themes/two/src/components/ProductDetails/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import { ProductMainDetails } from '../ProductMainDetails'
+import { useWindowSize } from '../../../../../hooks/useWindowSize'
+
+import {
+  Container
+} from './styles'
+export const ProductDetails = (props) => {
+  const {
+    open,
+    onClose,
+    product
+  } = props
+  const { width } = useWindowSize()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [extraOpen, setExtraOpen] = useState(false)
+  const [isExtendExtraOpen, setIsExtendExtraOpen] = useState(false)
+
+  const actionSidebar = (value) => {
+    setIsMenuOpen(value)
+
+    if (!value) {
+      props.onClose()
+    }
+  }
+
+  const toggleMainContent = () => {
+    if (isMenuOpen) {
+      if (width <= 500) {
+        document.getElementById('product_details').style.width = '100vw'
+      } else {
+        if (extraOpen && width >= 1000) {
+          document.getElementById('product_details').style.width = '1000px'
+        } else {
+          document.getElementById('product_details').style.width = '500px'
+        }
+      }
+    }
+  }
+  useEffect(() => {
+    if (width > 1000) {
+      setIsExtendExtraOpen(false)
+    }
+    toggleMainContent()
+  }, [width])
+
+  useEffect(() => {
+    if (!open) return
+    actionSidebar(true)
+  }, [open])
+
+  useEffect(() => {
+    if (width < 1000) return
+    if (extraOpen) {
+      document.getElementById('product_details').style.width = '1000px'
+    } else {
+      toggleMainContent()
+    }
+  }, [extraOpen])
+
+  useEffect(() => {
+    setExtraOpen(false)
+    setIsExtendExtraOpen(false)
+  }, [product])
+
+  return (
+    <Container id='product_details'>
+      <ProductMainDetails />
+    </Container>
+  )
+}
---
+++ src/themes/two/src/components/ProductMainDetails/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useLanguage, useUtils } from 'ordering-components-admin'
+
+export const ProductMainDetails = (props) => {
+  const {
+    product
+  } = props
+  return (
+    <>
+    </>
+  )
+}
