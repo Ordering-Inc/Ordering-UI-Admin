ab87c9977ad6953b84bc5ce7a55e5877b7cdc9e2
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-09-03 14:55:11 +0800

    update style

--- src/components/DropdownOptionList/styles.js
+++ src/components/DropdownOptionList/styles.js
@@ -37,7 +37,7 @@ export const DropdownOptionName = styled.div`
 `
 
 export const CityName = styled.div`
-  width: 20%;
+  width: 30%;
   font-size: 12px;
   box-sizing: border-box;
 
@@ -70,7 +70,7 @@ export const CityName = styled.div`
 
 export const CountryName = styled.div`
   font-size: 12px;
-  width: 30%;
+  width: 20%;
   box-sizing: border-box;
   div {
     border: none;
