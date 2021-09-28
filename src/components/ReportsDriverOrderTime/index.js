20253c8182d73fed525439be2698cbde1a776a41
Min <skyandarti@outlook.com>
2021-09-28 16:30:02 +0800

    Udpate: service times

--- src/components/ReportsDriverOrderTime/index.js
+++ src/components/ReportsDriverOrderTime/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react'
-import { useLanguage, useUtils } from 'ordering-components-admin'
+import { useLanguage } from 'ordering-components-admin'
 import Skeleton from 'react-loading-skeleton'
 import * as htmlToImage from 'html-to-image'
 import { ReportsDriverOrderTime as ReportsDriverOrderTimeController } from './naked'
@@ -35,7 +35,6 @@ const ReportsDriverOrderTimeUI = (props) => {
   const [, t] = useLanguage()
   const [isBusinessFilter, setIsBusinessFilter] = useState(false)
   const [isBrandFilter, setIsBrandFilter] = useState(false)
-  const [{ parseDate }] = useUtils()
 
   const tableRef = useRef(null)
 
@@ -113,15 +112,7 @@ const ReportsDriverOrderTimeUI = (props) => {
                   <Tbody key={i}>
                     <tr>
                       {tbody.map((td, j) => (
-                        <>
-                          {
-                            j === 0 ? (
-                              <td key={j} colSpan={td.colspan}>{td.value}</td>
-                            ) : (
-                              <td key={j} colSpan={td.colspan}>{td.value ? parseDate(new Date(td.value), { outputFormat: 'HH:mm:ss' }) : ''}</td>
-                            )
-                          }
-                        </>
+                        <td key={j} colSpan={td.colspan}>{td.value}</td>
                       ))}
                     </tr>
                   </Tbody>
