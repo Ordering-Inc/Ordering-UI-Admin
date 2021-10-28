20cb1e138713d80fbc27427934a1e7c490b6c470
Min <skyandarti@outlook.com>
2021-10-29 02:27:06 +0800

    Udpate: driver schedule

---
+++ src/components/ReportsDriverSchedule/index.js
@@ -0,0 +1,232 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { useLanguage } from 'ordering-components-admin'
+import { AdvancedReports as AdvancedReportsController } from './naked'
+import Skeleton from 'react-loading-skeleton'
+import { Button } from '../../styles/Buttons'
+import { Download } from 'react-bootstrap-icons'
+import { AnalyticsCalendar } from '../AnalyticsCalendar'
+import { Modal } from '../Modal'
+import { AnalyticsBusinessFilter } from '../AnalyticsBusinessFilter'
+import { ReportsBrandFilter } from '../ReportsBrandFilter'
+import { Alert } from '../Confirm'
+import {
+  ReportsBusinessSpendContainer,
+  Title,
+  ButtonActionList,
+  BrandBusinessWrapper,
+  CalendarWrapper,
+  DistancePerBrandWrapper,
+  DistanceTitleBlock,
+  DistanceTable,
+  Thead,
+  Tbody,
+  Tfoot,
+  TableWrapper,
+  EmptyContent
+} from './styles'
+
+const ReportsDriverScheduleUI = (props) => {
+  const {
+    filterList,
+    handleChangeFilterList,
+    reportData
+  } = props
+
+  const [, t] = useLanguage()
+  const [isBusinessFilter, setIsBusinessFilter] = useState(false)
+  const [isBrandFilter, setIsBrandFilter] = useState(false)
+  const [alertState, setAlertState] = useState({ open: false, content: [] })
+
+  const tableRef = useRef(null)
+
+  const handleChangeDate = (date1, date2) => {
+    handleChangeFilterList({ ...filterList, from: date1, to: date2 })
+  }
+
+  const closeAlert = () => {
+    setAlertState({
+      open: false,
+      content: []
+    })
+  }
+
+  const downloadCSV = () => {
+    if (reportData?.content?.body?.rows?.length === 0) return
+    let csv = ''
+    reportData.content.header.rows.forEach((tr) => {
+      tr.forEach((th) => {
+        csv += `${th.value},`
+        for (let i = 1; i < th.colspan; i++) {
+          csv += ' ,'
+        }
+      })
+      csv += '\n'
+    })
+    csv += '\n'
+    reportData.content.body.rows.forEach((tr) => {
+      tr.forEach((th) => {
+        csv += `${th.value},`
+        for (let i = 1; i < th.colspan; i++) {
+          csv += ' ,'
+        }
+      })
+      csv += '\n'
+    })
+    csv += '\n'
+    reportData.content.footer.rows.forEach((tr) => {
+      tr.forEach((th) => {
+        csv += `${th.value},`
+        for (let i = 1; i < th.colspan; i++) {
+          csv += ' ,'
+        }
+      })
+      csv += '\n'
+    })
+    csv += '\n'
+    var downloadLink = document.createElement('a')
+    var blob = new Blob(['\ufeff', csv])
+    var url = URL.createObjectURL(blob)
+    downloadLink.href = url
+    const fileSuffix = new Date().getTime()
+    downloadLink.download = `completed_orders_${fileSuffix}.csv`
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+    document.body.removeChild(downloadLink)
+  }
+
+  useEffect(() => {
+    if (reportData?.error) {
+      setAlertState({
+        open: true,
+        content: reportData?.error
+      })
+    }
+  }, [reportData?.error])
+
+  return (
+    <>
+      <ReportsBusinessSpendContainer>
+        <Title>{t('DRIVER_SCHEDULE', 'DRIVER SCHEDULE')}</Title>
+        <ButtonActionList>
+          <BrandBusinessWrapper>
+            <Button
+              onClick={() => setIsBrandFilter(true)}
+            >
+              {t('BRAND', 'Brand')} ({filterList?.franchises_id ? filterList?.franchises_id?.length : t('ALL', 'All')})
+            </Button>
+            <Button
+              onClick={() => setIsBusinessFilter(true)}
+            >
+              {t('BUSINESS', 'Business')} ({filterList?.businessIds ? filterList?.businessIds.length : t('ALL', 'All')})
+            </Button>
+          </BrandBusinessWrapper>
+          <CalendarWrapper>
+            <AnalyticsCalendar
+              handleChangeDate={handleChangeDate}
+              defaultValue={filterList}
+            />
+          </CalendarWrapper>
+        </ButtonActionList>
+        <DistancePerBrandWrapper>
+          <DistanceTitleBlock active={reportData?.content?.body?.rows?.length > 0}>
+            <h2>{t('DRIVER_SCHEDULE', 'DRIVER SCHEDULE')}</h2>
+            <Download onClick={() => downloadCSV()} />
+          </DistanceTitleBlock>
+          {reportData?.loading ? (
+            <div className='row'>
+              {[...Array(20).keys()].map(i => (
+                <div className='col-md-3 col-sm-3 col-3' key={i}><Skeleton /></div>
+              ))}
+            </div>
+          ) : (
+            <TableWrapper>
+              {reportData?.content?.body?.rows?.length > 0 ? (
+                <DistanceTable ref={tableRef}>
+                  {reportData?.content?.header?.rows.length > 0 && (
+                    <Thead>
+                      {
+                        reportData?.content?.header?.rows.map((tr, i) => (
+                          <tr key={i}>
+                            {tr?.map((th, j) => (
+                              <th key={j} colSpan={th.colspan}>{th.value}</th>
+                            ))}
+                          </tr>
+                        ))
+                      }
+                    </Thead>
+                  )}
+                  {reportData?.content?.body?.rows.map((tbody, i) => (
+                    <Tbody key={i}>
+                      <tr>
+                        {tbody.map((td, j) => (
+                          <td key={j} colSpan={td.colspan}>{td.value}</td>
+                        ))}
+                      </tr>
+                    </Tbody>
+                  ))}
+                  {reportData?.content?.footer?.rows.length > 0 && (
+                    <Tfoot>
+                      {
+                        reportData?.content?.footer?.rows.map((tr, i) => (
+                          <tr key={i}>
+                            {tr?.map((td, j) => (
+                              <td key={j} colSpan={td.colspan}>{td.value}</td>
+                            ))}
+                          </tr>
+                        ))
+                      }
+                    </Tfoot>
+                  )}
+                </DistanceTable>
+              ) : (
+                <EmptyContent>{t('NO_DATA', 'No Data')}</EmptyContent>
+              )}
+            </TableWrapper>
+          )}
+        </DistancePerBrandWrapper>
+        <Modal
+          width='50%'
+          height='80vh'
+          padding='30px'
+          title={t('BUSINESSES', 'Businesses')}
+          open={isBusinessFilter}
+          onClose={() => setIsBusinessFilter(false)}
+        >
+          <AnalyticsBusinessFilter
+            {...props} onClose={() => setIsBusinessFilter(false)}
+          />
+        </Modal>
+        <Modal
+          width='50%'
+          height='80vh'
+          padding='30px'
+          title={t('BRAND', 'Brand')}
+          open={isBrandFilter}
+          onClose={() => setIsBrandFilter(false)}
+        >
+          <ReportsBrandFilter
+            {...props} onClose={() => setIsBrandFilter(false)}
+          />
+        </Modal>
+      </ReportsBusinessSpendContainer>
+      <Alert
+        title={t('DRIVER_SCHEDULE', 'Driver schedule')}
+        content={alertState.content}
+        acceptText={t('ACCEPT', 'Accept')}
+        open={alertState.open}
+        onClose={() => closeAlert()}
+        onAccept={() => closeAlert()}
+        closeOnBackdrop={false}
+      />
+    </>
+  )
+}
+
+export const ReportsDriverSchedule = (props) => {
+  const reportsDriverScheduleProps = {
+    ...props,
+    UIComponent: ReportsDriverScheduleUI,
+    endpoint: 'drivers_operation_events'
+  }
+  return <AdvancedReportsController {...reportsDriverScheduleProps} />
+}
