ce0230acda9ee4243f0f8ca51540087e81e123ec
MingXue <mingxue@ordering.co>
2021-11-11 14:04:21 +0800

    add minimum container to allow item select

--- src/components/OrdersTable/styles.js
+++ src/components/OrdersTable/styles.js
@@ -6,7 +6,7 @@ export const OrdersContainer = styled.div`
   overflow: auto;
 
   @media (min-width: 768px) {
-    min-height: 350px;
+    min-height: 400px;
     max-height: calc(100vh - 385px);
   }
 
