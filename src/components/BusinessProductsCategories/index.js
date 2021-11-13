e2e338e4d9a61583b349f93c490c8805df8f2e0f
Carlos Gomez <carlos.g@ordering.co>
2021-11-13 11:45:56 -0600

    manage onclick on level 1

--- src/components/BusinessProductsCategories/index.js
+++ src/components/BusinessProductsCategories/index.js
@@ -49,7 +49,7 @@ export const BusinessProductsCategories = (props) => {
                         isSelected={categorySelected?.id === category.id}
                         isOpen={openCategories?.includes(category.id)}
                         spaceTab={categorySpace[category?.level ?? 1]}
-                        handleClickItem={(e) => onClickCategory(e, category)}
+                        handleClickItem={() => handlerClickCategory(category)}
                         IterateCategories={IterateCategories}
                       />
                     </div>
@@ -60,7 +60,7 @@ export const BusinessProductsCategories = (props) => {
                     active={categorySelected?.id === category.id}
                     className={`${category.id === 'featured' ? 'special' : ''}`}
                     categorySpace={categorySpace[category?.level ?? 1]}
-                    onClick={(e) => onClickCategory(e, category)}
+                    onClick={() => handlerClickCategory(category)}
                   >
                     <span>
                       {category.name}
@@ -88,7 +88,7 @@ export const BusinessProductsCategories = (props) => {
             active={categorySelected?.id === category.id}
             className={`${category.id === 'featured' ? 'special' : ''}`}
             categorySpace={categorySpace[category?.level ?? 1]}
-            onClick={(e) => onClickCategory(e, category)}
+            onClick={() => handlerClickCategory(category)}
           >
             <span>
               {currentCat.name}
