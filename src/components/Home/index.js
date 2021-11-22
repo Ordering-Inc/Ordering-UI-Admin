a6eec7cb8cbf5ad0f8d1621ee8dc13f367558e8e
MingXue <mingxue@ordering.co>
2021-11-22 21:57:08 +0800

    design change HomeScreen

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,45 +1,181 @@
-import React, { useState } from 'react'
-import { useLanguage, Home as HomeController } from 'ordering-components-admin'
+import React, { useState, useEffect } from 'react'
+import { useLanguage, useUtils, useEvent, Home as HomeController } from 'ordering-components-admin'
+import { Line } from 'react-chartjs-2'
+import moment from 'moment'
 import { IconButton, Button } from '../../styles/Buttons'
 import { useInfoShare } from '../../contexts/InfoShareContext'
+import Skeleton from 'react-loading-skeleton'
+import { useWindowSize } from '../../hooks/useWindowSize'
 import {
   List as MenuIcon,
-  DashSquare,
-  PlusSquare
+  Basket as OrdersIcon,
+  Cash as SalesIcon
 } from 'react-bootstrap-icons'
-import { HomeReports } from '../HomeReports'
-import { HomeSingleTask } from '../HomeSingleTask'
 import {
   HomeContainer,
+  Breadcrumb,
   HeaderContainer,
   WelcomeMsg,
   GuideMsg,
-  VideoAndReportsWrapper,
-  HideAndShowWrapper,
-  VideoOnBoardingWrapper,
-  VideoOnBoardingContainer,
-  IframeWrapper,
-  FirstStoreToSellWrapper,
-  FirstStoreToSellContent,
-  OrderingHelpButton
+  Reports,
+  SalesGraphContainer,
+  ChartHeaderContainer,
+  OrdersAndSalesWrapper,
+  OrdersOrSalesContainer,
+  DetailsContent,
+  BoxIconContainer,
+  ParagraphHeaders,
+  AssistanceWidgets,
+  AssistanceTitle,
+  AssistanceBody,
+  WidgeBlock
 } from './styles'
 
 const HomeUI = (props) => {
   const {
-    taskList
+    ordersList,
+    todaySalelsList,
+    monthSalesList,
+    getCurrentDateRange
   } = props
   const [, t] = useLanguage()
   const [{ isCollapse }, { handleMenuCollapse }] = useInfoShare()
-  const [isShowVideo, setIsShowVideo] = useState(true)
-  const [isShowStore, setIsShowStore] = useState(true)
+  const [timeAxes, setTimeAxes] = useState([])
+  const [events] = useEvent()
+  const { width } = useWindowSize()
+  const [{ parsePrice }] = useUtils()
 
-  const goToContactUs = () => {
-    window.open('https://www.ordering.co/en-us/contact-us', '_blank')
+  const goToContact = (location) => {
+    if (location === 'sales') {
+      window.open('https://www.ordering.co/ordering-sales', '_blank')
+    }
+    if (location === 'tech') {
+      window.open('https://www.ordering.co/ordering-support', '_blank')
+    }
   }
+
+  const generateData = () => {
+    const values = []
+    timeAxes.forEach(function (axe) {
+      const index = monthSalesList?.sales?.findIndex(history => {
+        return axe === history?.time
+      })
+      if (index !== -1) {
+        values.push({ x: monthSalesList?.sales[index]?.time, y: monthSalesList?.sales[index]?.sales })
+      } else {
+        values.push({ x: axe, y: 0 })
+      }
+    })
+    return values
+  }
+
+  const handleGoToPage = (data) => {
+    events.emit('go_to_page', data)
+  }
+
+  const updateTimeAxes = () => {
+    var unitdate = getTimeAxes(getCurrentDateRange()).time.unit
+    var maxdate = moment(getTimeAxes(getCurrentDateRange()).time.max).endOf(unitdate)
+    var mindate = moment(getTimeAxes(getCurrentDateRange()).time.min).startOf(unitdate)
+    var curDate = mindate
+    var newTimeAxes = []
+    var adder = 'd'
+    switch (unitdate) {
+      case 'day': adder = 'd'
+        break
+      case 'month': adder = 'M'
+        break
+      case 'year': adder = 'y'
+        break
+      case 'hour': adder = 'h'
+        break
+      default: break
+    }
+    do {
+      newTimeAxes.push(moment(curDate._d.getTime()).format('YYYY-MM-DD HH:mm:ss'))
+      curDate = curDate.clone().add(1, adder)
+    }
+    while (curDate <= maxdate)
+    setTimeAxes(newTimeAxes)
+  }
+
+  const getTimeAxes = (lapse) => {
+    const xAxes = {
+      type: 'time',
+      grid: {
+        display: false
+      },
+      time: {
+        stepSize: 1,
+        displayFormats: {
+          hour: 'LT'
+        }
+      },
+      ticks: {
+        fontSize: 12,
+        fontColor: '#CED4DA'
+      }
+    }
+
+    xAxes.time.unit = 'day'
+    xAxes.time.min = moment().subtract(30, 'days').format('YYYY-MM-DD')
+    xAxes.time.max = moment().format('YYYY-MM-DD')
+    return xAxes
+  }
+
+  const defaultData = {
+    datasets: [
+      {
+        data: generateData(),
+        fill: false,
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: '#2C7BE5',
+        tension: 0.4,
+        borderWidth: 3
+      }
+    ]
+  }
+
+  const options = {
+    scales: {
+      x: getTimeAxes(),
+      y: {
+        beginAtZero: true,
+        grid: {
+          drawBorder: false
+        },
+        ticks: {
+          font: {
+            size: 11,
+            color: '#CED4DA'
+          }
+        }
+      }
+    },
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+    pointRadius: 0
+  }
+
+  const getTotalSales = () => {
+    let data = 0
+    for (const sale of monthSalesList?.sales) {
+      data += sale.sales
+    }
+
+    return parsePrice(data, { separator: '.' })
+  }
+
+  useEffect(() => {
+    updateTimeAxes()
+  }, [])
 
   return (
     <HomeContainer>
-      <HeaderContainer>
+      <Breadcrumb>
         {isCollapse && (
           <IconButton
             color='black'
@@ -49,76 +185,80 @@ const HomeUI = (props) => {
           </IconButton>
         )}
         <h1>{t('HOME', 'Home')}</h1>
+      </Breadcrumb>
+      <HeaderContainer>
+        <WelcomeMsg>{t('WELCOME_TO_ORDERING', 'Welcome to Ordering')}!</WelcomeMsg>
+        <GuideMsg>{t('ORDERING_GUIDE_MSG', 'Our guide helps you to configure your Ordering products.')}</GuideMsg>
       </HeaderContainer>
-      <WelcomeMsg>{t('WELCOME_TO_ORDERING', 'Welcome to Ordering')}!</WelcomeMsg>
-      <GuideMsg>{t('ORDERING_GUIDE_MSG', 'Our guide helps you to configure your Ordering products.')}</GuideMsg>
-      <VideoAndReportsWrapper isRowDirection={isShowVideo}>
-        <VideoOnBoardingWrapper>
-          <HideAndShowWrapper>
-            <span>{t('VIDEO_ONBOARDING', 'Video onboarding')}</span>
-            <div onClick={() => setIsShowVideo(!isShowVideo)}>
-              {
-                isShowVideo ? <DashSquare /> : <PlusSquare />
-              }
-            </div>
-          </HideAndShowWrapper>
-          {
-            isShowVideo && (
-              <VideoOnBoardingContainer>
-                <IframeWrapper
-                  dangerouslySetInnerHTML={{ __html: '<iframe src="https://www.youtube-nocookie.com/embed/Bi8WLLRzZ1g" frameborder="0" loading="lazy" allowfullscreen />' }}
-                />
-              </VideoOnBoardingContainer>
-            )
-          }
-        </VideoOnBoardingWrapper>
-        <HomeReports
-          {...props}
-          isShowVideo={isShowVideo}
-        />
-      </VideoAndReportsWrapper>
-      <FirstStoreToSellWrapper>
-        <HideAndShowWrapper className='home-store-title'>
-          <span>{t('GET_YOUR_FIRST_STORE_READY_TO_SELL', 'Get your first store ready to sell.')}</span>
-          <div onClick={() => setIsShowStore(!isShowStore)}>
+      <ParagraphHeaders>
+        <p>{t('REPORTS', 'Reports')}</p>
+        <Button
+          color='lightPrimary'
+          onClick={() => handleGoToPage({ page: 'business_analytics' })}
+        >
+          {t('SEE_MORE_REPORTS', 'See more reports')}
+        </Button>
+      </ParagraphHeaders>
+      <Reports>
+        <SalesGraphContainer>
+          <ChartHeaderContainer>
+            <p>{t('THIS_MONTH_SALES', 'This month sales')}</p>
             {
-              isShowStore ? <DashSquare /> : <PlusSquare />
+              monthSalesList?.loading ? <Skeleton width={70} height={30} /> : <h3>{getTotalSales()}</h3>
             }
-          </div>
-        </HideAndShowWrapper>
-        <FirstStoreToSellContent active={isShowStore}>
+          </ChartHeaderContainer>
           {
-            taskList?.loading ? (
-              <>
-                {
-                  [...Array(5).keys()].map(i => (
-                    <HomeSingleTask
-                      key={i}
-                      isSkeleton
-                    />
-                  ))
-                }
-              </>
-            ) : (
-              <>
-                {
-                  taskList?.data?.length > 0 && taskList?.data.map((task, i) => (
-                    <HomeSingleTask
-                      {...props}
-                      key={i}
-                      task={task}
-                    />
-                  ))
-                }
-              </>
-            )
+            monthSalesList?.loading ? <Skeleton height={150} /> : <Line data={defaultData} options={options} height={width > 1400 ? '106px' : ''} />
           }
-          <OrderingHelpButton>
-            <p>{t('ORDERING_HELP_CONDITION_LINK', 'If you need anything else from your Ordering Team..')}</p>
-            <Button color='lightPrimary' borderRadius='7.6px' onClick={goToContactUs}>{t('CLICK_HERE', 'Click here')}!</Button>
-          </OrderingHelpButton>
-        </FirstStoreToSellContent>
-      </FirstStoreToSellWrapper>
+        </SalesGraphContainer>
+        <OrdersAndSalesWrapper>
+          <OrdersOrSalesContainer>
+            <DetailsContent>
+              <p>{t('TODAY_ORDERS', 'Today Orders')}</p>
+              {
+                ordersList?.loading ? <Skeleton width={70} height={30} /> : <h2>{ordersList?.orders}</h2>
+              }
+            </DetailsContent>
+            <BoxIconContainer>
+              <OrdersIcon />
+            </BoxIconContainer>
+          </OrdersOrSalesContainer>
+          <OrdersOrSalesContainer>
+            <DetailsContent>
+              <p>{t('TODAY_SALES', 'Today Sales')}</p>
+              {
+                todaySalelsList?.loading ? <Skeleton width={70} height={30} /> : <h2>{parsePrice(todaySalelsList?.sales, { separator: '.' })}</h2>
+              }
+            </DetailsContent>
+            <BoxIconContainer isSales>
+              <SalesIcon />
+            </BoxIconContainer>
+          </OrdersOrSalesContainer>
+        </OrdersAndSalesWrapper>
+      </Reports>
+      <ParagraphHeaders>
+        <p>{t('SUPPORT', 'Support')}</p>
+      </ParagraphHeaders>
+      <AssistanceWidgets>
+        <AssistanceTitle>
+          <h1>{t('ASSIST_TITILE_1', 'Which kind of')}{' '}
+            <span>{t('ASSIST_TITILE_2', 'assistance do you need?')}</span>
+          </h1>
+          <p>{t('ASSIST_SUB_TITILE', 'Choose the asistance you are looking for in the buttons below.')}</p>
+        </AssistanceTitle>
+        <AssistanceBody>
+          <WidgeBlock>
+            <h3>{t('CONTACT_SALES_TITLE', 'Contact our Sales Team')}</h3>
+            <p>{t('CONTACT_SALES_SUB_TITLE', 'Ask about pricing, custom work, budget and more money talk')}</p>
+            <Button outline color='primary' onClick={() => goToContact('sales')}>{t('CONTACT_SALES_BUTTON_TEXT', 'Sales Contact')}</Button>
+          </WidgeBlock>
+          <WidgeBlock>
+            <h3>{t('CONTACT_SUPPORT_TITLE', 'Contact our Support Team')}</h3>
+            <p>{t('CONTACT_SUPPORT_SUB_TITLE', 'Ask about your ordering installation, products and features')}</p>
+            <Button outline color='primary' onClick={() => goToContact('tech')}>{t('CONTACT_SUPPORT_BUTTON_TEXT', 'Tech Support')}</Button>
+          </WidgeBlock>
+        </AssistanceBody>
+      </AssistanceWidgets>
     </HomeContainer>
   )
 }
