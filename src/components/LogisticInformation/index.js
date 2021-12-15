19b9c1148aa8118be37a0fa453e0dd86abd95c23
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2021-12-15 18:12:25 +0800

    update order logistic info

--- src/components/LogisticInformation/index.js
+++ src/components/LogisticInformation/index.js
@@ -14,37 +14,35 @@ const LogisticInformationUI = (props) => {
   const [, t] = useLanguage()
 
   const [{ parseDate, parseDistance }] = useUtils()
-  const getStatus = (status) => {
-    switch (status) {
-      case 0:
-        return 'Pending order'
-      case 1:
-        return 'Completed by admin'
-      case 2:
-        return 'Reject by admin'
-      case 3:
-        return 'Driver arrived by business'
-      case 4:
-        return 'Ready for pickup'
-      case 5:
-        return 'Reject by business'
-      case 6:
-        return 'Reject by driver'
-      case 7:
-        return 'Accepted by business'
-      case 8:
-        return 'Accepted by driver'
-      case 9:
-        return 'Pickup completed by driver'
-      case 10:
-        return 'Pickup failed by driver'
-      case 11:
-        return 'Delivery completed by driver'
-      case 12:
-        return 'Delivery failed by driver'
-      default:
-        return status
-    }
+
+  const getOrderStatus = (status) => {
+    const orderStatus = [
+      { key: 0, value: t('PENDING', 'Pending') },
+      { key: 1, value: t('COMPLETED_BY_ADMIN', 'Completed by admin') },
+      { key: 2, value: t('REJECTED_BY_ADMIN', 'Rejected by admin') },
+      { key: 3, value: t('ORDER_STATUS_IN_BUSINESS', 'Driver arrived to business') },
+      { key: 4, value: t('PREPARATION_COMPLETED', 'Preparation Completed') },
+      { key: 5, value: t('REJECTED_BY_BUSINESS', 'Rejected by business') },
+      { key: 6, value: t('REJECTED_BY_DRIVER', 'Rejected by driver') },
+      { key: 7, value: t('ACCEPTED_BY_BUSINESS', 'Accepted by business') },
+      { key: 8, value: t('ACCEPTED_BY_DRIVER', 'Accepted by driver') },
+      { key: 9, value: t('PICK_UP_COMPLETED_BY_DRIVER', 'Pick up completed by driver') },
+      { key: 10, value: t('PICK_UP_FAILED_BY_DRIVER', 'Pick up Failed by driver') },
+      { key: 11, value: t('DELIVERY_COMPLETED_BY_DRIVER', 'Delivery completed by driver') },
+      { key: 12, value: t('DELIVERY_FAILED_BY_DRIVER', 'Delivery Failed by driver') },
+      { key: 13, value: t('PREORDER', 'Preorder') },
+      { key: 14, value: t('ORDER_NOT_READY', 'Order not ready') },
+      { key: 15, value: t('ORDER_PICKEDUP_COMPLETED_BY_CUSTOMER', 'Pickup completed by customer') },
+      { key: 16, value: t('ORDER_STATUS_CANCELLED_BY_CUSTOMER', 'Cancelled by customer') },
+      { key: 17, value: t('ORDER_NOT_PICKEDUP_BY_CUSTOMER', 'Not picked by customer') },
+      { key: 18, value: t('ORDER_DRIVER_ALMOST_ARRIVED_BUSINESS', 'Driver almost arrived to business') },
+      { key: 19, value: t('ORDER_DRIVER_ALMOST_ARRIVED_CUSTOMER', 'Driver almost arrived to customer') },
+      { key: 20, value: t('ORDER_CUSTOMER_ALMOST_ARRIVED_BUSINESS', 'Customer almost arrived to business') },
+      { key: 21, value: t('ORDER_CUSTOMER_ARRIVED_BUSINESS', 'Customer arrived to business') }
+    ]
+
+    const objectStatus = orderStatus.find((o) => o.key === status)
+    return objectStatus && objectStatus
   }
 
   return (
@@ -73,8 +71,8 @@ const LogisticInformationUI = (props) => {
                 {t('RELOAD', 'Reload')}
               </Button>
               <BubbleConsole>
-                <UppercaseText><strong>{t('distance_customer_from_business', 'distance_customer_from_business')}</strong></UppercaseText>
-                : {parseDistance((logisticInformation?.data?.distance_customer_from_business / 1000).toFixed(2))}
+                <UppercaseText><strong>{t('DISTANCE_CUSTOMER_FROM_BUSINESS', 'DISTANCE FROM CUSTOMER TO BUSINESS')}</strong></UppercaseText>
+                : {parseDistance((logisticInformation?.data?.distance_customer_from_business / 1000))}
               </BubbleConsole>
               <>
                 <BubbleConsole>
@@ -89,37 +87,31 @@ const LogisticInformationUI = (props) => {
                             <span>{i + 1}. {queue.name}</span>
                           </BubbleConsole>
                         )}
-                        {queue?.drivers.length > 0 && (
-                          <BubbleConsole>
-                            <strong>{t('AVAILABLE DRIVERS', 'Available drivers')}: </strong>
-                            <span>{queue.drivers.length}</span>
-                          </BubbleConsole>
-                        )}
-                        {queue?.priority && (
-                          <BubbleConsole>
-                            <strong>{t('PRIORITY', 'Priority')}: </strong>
-                            <span>{queue.priority}</span>
-                          </BubbleConsole>
-                        )}
-                        {queue?.orders_group_start_in_status && (
-                          <BubbleConsole>
-                            <strong>{t('START_IN', 'Start in')}: </strong>
-                            <span>{getStatus(queue.orders_group_start_in_status)}</span>
-                          </BubbleConsole>
-                        )}
                         <BubbleConsole>
-                          {queue.orders.length > 0 ? (
-                            <>
-                              {queue.orders.map((order, i) => (
-                                <strong key={order.id}>
-                                  {i + 1}. {order.id} {order?.waiting_for_preorder && <span>({t('waiting_for_preorder', 'waiting_for_preorder')})</span>}
-                                </strong>
-                              ))}
-                            </>
-                          ) : (
-                            t('WITHOUT_ORDERS', 'without_orders')
-                          )}
+                          <strong>{t('AVAILABLE DRIVERS', 'Available drivers')}: </strong>
+                          <span>{queue.drivers.length}</span>
                         </BubbleConsole>
+                        <BubbleConsole>
+                          <strong>{t('PRIORITY', 'Priority')}: </strong>
+                          <span>{queue.priority}</span>
+                        </BubbleConsole>
+                        <BubbleConsole>
+                          <strong>{t('START_IN', 'Start in')}: </strong>
+                          <span>{getOrderStatus(queue.orders_group_start_in_status)?.value}</span>
+                        </BubbleConsole>
+                        {queue.orders.length > 0 ? (
+                          <BubbleConsole>
+                            {queue.orders.map((order, i) => (
+                              <div key={order.id}>
+                                {i + 1}. {order.id} {order?.waiting_for_preorder && <span>({t('waiting_for_preorder', 'waiting_for_preorder')})</span>}
+                              </div>
+                            ))}
+                          </BubbleConsole>
+                        ) : (
+                          <BubbleConsole>
+                            {t('WITHOUT_ORDERS', 'without_orders')}
+                          </BubbleConsole>
+                        )}
                       </React.Fragment>
                     ))}
                   </>
