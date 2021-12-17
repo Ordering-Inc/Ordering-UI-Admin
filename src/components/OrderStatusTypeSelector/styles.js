be2b3debd46ebd8ad1dd990dbbdb7f918e93e992
MingXue <mingxue@ordering.co>
2021-12-18 04:08:08 +0800

    add orders manager change status

--- src/components/OrderStatusTypeSelector/styles.js
+++ src/components/OrderStatusTypeSelector/styles.js
@@ -1,5 +1,24 @@
 import styled, { css } from 'styled-components'
 
+export const OrderStatusTypeSelectWrapper = styled.div`
+  .list-wrapper {
+    min-width: 297px;
+  }
+
+  .orderStatus {
+    width: 100%;
+  }
+
+  .search-bar-container {
+    padding: 10px 11px;
+    width: 100%;
+    input {
+      border-radius: 7.6px;
+      width: 100%;
+    }
+  }
+`
+
 export const Option = styled.div`
   display: flex;
   align-items: center;
