ec7235aae32084aed460f59a3fcca8345002597c
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-12-21 20:37:54 -0400

    update sentry key (#184)

--- index-template.js
+++ index-template.js
@@ -97,25 +97,27 @@ import orderStatus21 from './template/assets/images/order/status-21.svg'
 
 import categoryAll from './template/assets/images/categories/category-all.png'
 
-Sentry.init({
-  environment: window?.location?.hostname === 'localhost' ? 'development' : process.env.NODE_ENV,
-  dsn: 'https://136774353f6d423da02589e771165836@o460529.ingest.sentry.io/5681485',
-  integrations: [
-    new Integrations.BrowserTracing()
-  ],
-  release: process.env.npm_package_version ? 'ordering-ui-admin@' + process.env.npm_package_version : 'ordering-ui-admin@' + '0.0.2',
-  ignoreErrors: [
-    'is not defined',
-    'is not a function',
-    'can\'t find variable',
-    'objects are not valid',
-    'element type is invalid'
-  ],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: window?.location?.hostname === 'localhost' ? 0 : 0.5
-})
+if (!(window?.location?.hostname === 'localhost')) {
+  Sentry.init({
+    environment: process.env.NODE_ENV,
+    dsn: 'https://84af7231923c4a298cc81c2d8d8365c7@o460529.ingest.sentry.io/5681485',
+    integrations: [
+      new Integrations.BrowserTracing()
+    ],
+    release: process.env.npm_package_version ? 'ordering-ui-admin@' + process.env.npm_package_version : 'ordering-ui-admin@' + '0.0.2',
+    ignoreErrors: [
+      'is not defined',
+      'is not a function',
+      'can\'t find variable',
+      'objects are not valid',
+      'element type is invalid'
+    ],
+  
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 0.2
+  })
+}
 
 const logos = {
   logotype,
