1c12c988345bdf7ec97e0899868d232f6b79050d
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-02-09 17:26:28 +0800

    update

---
+++ src/components/Users/BusinessManagerBusinesses/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { BusinessManagerBusinesses as BusinessManagerBusinessesController } from './naked'
+import { SelectBusinesses } from '../../Shared'
+
+const BusinessManagerBusinessesUI = (props) => {
+  const {
+    actionState,
+    selectedBusinessIds,
+    businessesState,
+    handleSelectBusiness,
+    handleSelectAllBusiness,
+    handleUpdateBusinesses
+  } = props
+
+  return (
+    <>
+      <SelectBusinesses
+        isSkeleton={businessesState.loading}
+        allBusinesses={businessesState.businesses}
+        selectedBusinessIds={selectedBusinessIds}
+        handleSelectBusiness={handleSelectBusiness}
+        handleSelectAllBusinesses={() => handleSelectAllBusiness(true)}
+        handleSelectNoneBusinesses={() => handleSelectAllBusiness(false)}
+        isDisabled={actionState.loading || businessesState.loading}
+        handleUpdateClick={handleUpdateBusinesses}
+      />
+    </>
+  )
+}
+
+export const BusinessManagerBusinesses = (props) => {
+  const businessMenuShareProps = {
+    ...props,
+    UIComponent: BusinessManagerBusinessesUI
+  }
+  return (
+    <BusinessManagerBusinessesController {...businessMenuShareProps} />
+  )
+}
