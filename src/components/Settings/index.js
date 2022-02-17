04c7c348f132d17bd68d720238f4399d32d46fab
carlosg-ordering <68757729+carlosg-ordering@users.noreply.github.com>
2022-02-17 13:57:23 -0600

    [FIX] Missing imports (#266)

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -3,11 +3,13 @@ import { IntegrationListing } from './IntegrationListing'
 import { LanguageManager } from './LanguageManager'
 import { PlaceListing } from './PlaceListing'
 import { Settings } from './Settings'
+import { SettingItemUI } from './SettingItemUI'
 
 export {
   Cms,
   IntegrationListing,
   LanguageManager,
   PlaceListing,
-  Settings
+  Settings,
+  SettingItemUI
 }
