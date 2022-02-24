28c4682f7dddd6cd5bdb19b107bce862e623ad0a
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-02-25 01:49:36 +0800

    change icon

--- src/components/SidebarMenu/index.js
+++ src/components/SidebarMenu/index.js
@@ -14,7 +14,8 @@ import {
   Truck,
   GraphUp,
   WindowDock,
-  Award
+  Award,
+  BoxArrowUpRight
 } from 'react-bootstrap-icons'
 import { useTheme } from 'styled-components'
 import { useEvent, useLanguage, useSession } from 'ordering-components-admin'
@@ -565,10 +566,7 @@ export const SidebarMenu = (props) => {
                   variant={false}
                   onClick={() => handleGoToLink('https://apps.tryordering.com/store/marketplace')}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="0.8em" height="0.8em" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
-                    <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
-                  </svg>
+                  <BoxArrowUpRight />
                   <span>{t('MARKETPLACE', 'Marketplace')}</span>
                 </Button>
               </div>
