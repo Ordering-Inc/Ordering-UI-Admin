d41cebfed4727404bbbc56458ecf40e3e3b726fb
carlosq-ordering <carlos.q@ordering.co>
2022-02-24 10:33:49 -0400

    build

--- _modules/components/SidebarMenu/index.js
+++ _modules/components/SidebarMenu/index.js
@@ -213,6 +213,10 @@ var SidebarMenu = function SidebarMenu(props) {
     events.emit('go_to_page', data);
   };
 
+  var handleGoToLink = function handleGoToLink(link) {
+    window.open(link, '_blank');
+  };
+
   (0, _react.useEffect)(function () {
     if (windowSize.width < 1024) {
       handleMenuCollapse(true);
@@ -397,7 +401,26 @@ var SidebarMenu = function SidebarMenu(props) {
         page: 'support'
       });
     }
-  }, /*#__PURE__*/_react.default.createElement(_reactBootstrapIcons.Headset, null), /*#__PURE__*/_react.default.createElement("span", null, t('SUPPORT', 'Support'))))), /*#__PURE__*/_react.default.createElement(_styles.UserInfo, {
+  }, /*#__PURE__*/_react.default.createElement(_reactBootstrapIcons.Headset, null), /*#__PURE__*/_react.default.createElement("span", null, t('SUPPORT', 'Support'))), /*#__PURE__*/_react.default.createElement(_reactBootstrap.Button, {
+    className: "d-flex align-items-center",
+    variant: false,
+    onClick: function onClick() {
+      return handleGoToLink('https://apps.tryordering.com/store/marketplace');
+    }
+  }, /*#__PURE__*/_react.default.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "0.8em",
+    height: "0.8em",
+    fill: "currentColor",
+    class: "bi bi-box-arrow-up-right",
+    viewBox: "0 0 16 16"
+  }, /*#__PURE__*/_react.default.createElement("path", {
+    "fill-rule": "evenodd",
+    d: "M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    "fill-rule": "evenodd",
+    d: "M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"
+  })), /*#__PURE__*/_react.default.createElement("span", null, t('MARKETPLACE', 'Marketplace'))))), /*#__PURE__*/_react.default.createElement(_styles.UserInfo, {
     id: "user_info",
     className: "d-flex flex-column px-1"
   }, /*#__PURE__*/_react.default.createElement(_reactBootstrap.Button, {
