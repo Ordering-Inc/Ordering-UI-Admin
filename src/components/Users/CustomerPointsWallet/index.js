81f1aab4cbf9319cd443a8f38377b0ec95f86db8
Min <skyandarti@outlook.com>
2022-03-09 03:32:18 +0800

    Update: points wallet

--- src/components/Users/CustomerPointsWallet/index.js
+++ src/components/Users/CustomerPointsWallet/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useLanguage, useUtils, CustomerPointsWallet as CustomerPointsWalletController } from 'ordering-components-admin'
+import { useLanguage, CustomerPointsWallet as CustomerPointsWalletController } from 'ordering-components-admin'
 import Skeleton from 'react-loading-skeleton'
 import {
   Container,
   PointsWalletContainer,
-  PointsWrapper,
-  EqualWrapper
+  PointsWrapper
+  // EqualWrapper
 } from './styles'
 
 const CustomerPointsWalletUI = (props) => {
@@ -14,7 +14,7 @@ const CustomerPointsWalletUI = (props) => {
   } = props
 
   const [, t] = useLanguage()
-  const [{ parsePrice }] = useUtils()
+  // const [{ parsePrice }] = useUtils()
 
   return (
     <Container>
@@ -26,11 +26,11 @@ const CustomerPointsWalletUI = (props) => {
               <h2><Skeleton width={70} height={20} /></h2>
               <p><Skeleton width={30} height={15} /></p>
             </PointsWrapper>
-            <EqualWrapper><Skeleton width={30} height={20} /></EqualWrapper>
+            {/* <EqualWrapper><Skeleton width={30} height={20} /></EqualWrapper>
             <PointsWrapper>
               <h2><Skeleton width={70} height={20} /></h2>
               <p><Skeleton width={30} height={15} /></p>
-            </PointsWrapper>
+            </PointsWrapper> */}
           </PointsWalletContainer>
         </>
       ) : (
@@ -40,11 +40,11 @@ const CustomerPointsWalletUI = (props) => {
               <h2>{WalletState.wallets?.find(wallet => wallet.type === 'credit_point')?.balance || 0}</h2>
               <p>{t('POINTS', 'Points')}</p>
             </PointsWrapper>
-            <EqualWrapper>,</EqualWrapper>
+            {/* <EqualWrapper>,</EqualWrapper>
             <PointsWrapper>
               <h2>{parsePrice(WalletState.wallets?.find(wallet => wallet.type === 'cash')?.balance || 0)}</h2>
               <p>{t('CASH', 'Cash')}</p>
-            </PointsWrapper>
+            </PointsWrapper> */}
           </PointsWalletContainer>
         </>
       )}
