eca2c9a695e306707a669e8a8fbecbcf4da62e96
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-03-11 23:40:34 +0800

    hide drivers dashboard for business owner

--- src/components/SidebarMenu/index.js
+++ src/components/SidebarMenu/index.js
@@ -305,6 +305,7 @@ export const SidebarMenu = (props) => {
                     <Accordion.Collapse eventKey='1'>
                       <MenuContent>
                         {ordersSubMenus.map(item => (
+                          !(sessionState?.user?.level === 2 && item.pageName === 'drivers') &&
                           !(sessionState?.user?.level === 5 && item.pageName === 'drivers') &&
                           !(sessionState?.user?.level === 5 && item.pageName === 'deliveries') && (
                             <SubMenu
