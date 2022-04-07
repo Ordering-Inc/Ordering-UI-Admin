3f315c25c4239c0e02816b77cc7346d61f9685ce
Carlos Gomez <carlos.g@ordering.co>
2022-04-07 16:43:01 -0500

    build

--- _modules/components/Marketing/EnterprisePromotionConditions/styles.js
+++ _modules/components/Marketing/EnterprisePromotionConditions/styles.js
@@ -9,7 +9,7 @@ exports.Header = exports.EditButton = exports.ConditionsContainer = exports.Cond
 
 var _styledComponents = _interopRequireWildcard(require("styled-components"));
 
-var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5, _templateObject6, _templateObject7, _templateObject8, _templateObject9;
+var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5, _templateObject6, _templateObject7, _templateObject8, _templateObject9, _templateObject10;
 
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
@@ -43,11 +43,14 @@ var EditButton = _styledComponents.default.span(_templateObject6 || (_templateOb
 
 exports.EditButton = EditButton;
 
-var CheckboxWrapper = _styledComponents.default.div(_templateObject7 || (_templateObject7 = _taggedTemplateLiteral(["\n  width: 18px;\n  height: 18px;\n  border-radius: 2px;\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  svg {\n    color: #FFF;\n    font-size: 16px;\n  }\n  cursor: not-allowed;\n\n  ", "\n"])), function (_ref) {
-  var active = _ref.active;
-  return active ? (0, _styledComponents.css)(_templateObject8 || (_templateObject8 = _taggedTemplateLiteral(["\n    /* cursor: pointer; */\n    background: ", ";\n  "])), function (props) {
+var CheckboxWrapper = _styledComponents.default.div(_templateObject7 || (_templateObject7 = _taggedTemplateLiteral(["\n  width: 18px;\n  height: 18px;\n  border-radius: 2px;\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  svg {\n    color: #FFF;\n    font-size: 16px;\n  }\n  ", "\n\n  ", "\n"])), function (_ref) {
+  var isCursorAllowed = _ref.isCursorAllowed;
+  return !isCursorAllowed && (0, _styledComponents.css)(_templateObject8 || (_templateObject8 = _taggedTemplateLiteral(["\n    cursor: not-allowed;\n  "])));
+}, function (_ref2) {
+  var active = _ref2.active;
+  return active ? (0, _styledComponents.css)(_templateObject9 || (_templateObject9 = _taggedTemplateLiteral(["\n    /* cursor: pointer; */\n    background: ", ";\n  "])), function (props) {
     return props.theme.colors.primary;
-  }) : (0, _styledComponents.css)(_templateObject9 || (_templateObject9 = _taggedTemplateLiteral(["\n    border: 1px solid #B1BCCC;\n  "])));
+  }) : (0, _styledComponents.css)(_templateObject10 || (_templateObject10 = _taggedTemplateLiteral(["\n    border: 1px solid #B1BCCC;\n  "])));
 });
 
 exports.CheckboxWrapper = CheckboxWrapper;
