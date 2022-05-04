a5761e921491691fef8ccfb3f6676084d9413578
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-05-05 04:21:11 +0800

    [Add] Stripe link

--- src/components/Orders/OrderDetailsHeader/index.js
+++ src/components/Orders/OrderDetailsHeader/index.js
@@ -15,7 +15,8 @@ import { IconButton as ButtonLink } from '../../../styles'
 import {
   OrderDetailsHeaderContainer,
   UreadMessageAlert,
-  ButtonGroup
+  ButtonGroup,
+  StripeLink
 } from './styles'
 
 export const OrderDetailsHeader = (props) => {
@@ -35,6 +36,8 @@ export const OrderDetailsHeader = (props) => {
   const [, t] = useLanguage()
   const [{ user }] = useSession()
 
+  const stripePaymethods = ['stripe', 'stripe_direct', 'stripe_connect', 'stripe_redirect']
+
   const walletName = {
     cash: {
       name: t('CASH_WALLET', 'Cash Wallet')
@@ -53,108 +56,110 @@ export const OrderDetailsHeader = (props) => {
     <OrderDetailsHeaderContainer>
       <div>
         <h1>{t('INVOICE_ORDER_NO', 'Order No')} {order?.id}</h1>
-        <p>
-          {order?.payment_events?.length > 0 ? (
-            order?.payment_events?.map((event, i) => (
-              <React.Fragment key={i}>
-                <span>
-                  {event?.wallet_event
-                    ? walletName[event?.wallet_event?.wallet?.type]?.name
-                    : event?.paymethod?.name}
-                </span>
-                <EnDotSingle />
-              </React.Fragment>
-            ))
-          ) : (
-            <>
-              <span>{order?.paymethod?.name}</span>
-              <EnDotSingle />
-            </>
-          )}
-          {order?.delivery_type === 1 && (
-            <span>
-              {t('DELIVERY', 'Delivery')}
-            </span>
-          )}
-          {order?.delivery_type === 2 && (
-            <span>
-              {t('PICKUP', 'Pickup')}
-            </span>
-          )}
-          {order?.delivery_type === 3 && (
-            <span>
-              {t('EAT_IN', 'Eat in')}
-            </span>
-          )}
-          {order?.delivery_type === 4 && (
-            <span>
-              {t('CURBSIDE', 'Curbside')}
-            </span>
-          )}
-          {order?.delivery_type === 5 && (
-            <span>
-              {t('DRIVE_THRU', 'Drive thru')}
-            </span>
-          )}
-        </p>
-      </div>
-      <ButtonGroup>
-        {/* <ButtonLink
-          isDisabled={isTourOpen && currentTourStep === 1}
-          onClick={() => handleShowOption('cash_wallet')}
-        >
-          <Wallet2 />
-        </ButtonLink> */}
-        {user?.level !== 5 && (
-          <ButtonLink
-            color='black'
-            active={openMessage?.chat}
-            onClick={() => handleOpenMessages('chat')}
-            isDisabled={isTourOpen && currentTourStep === 1}
-          >
-            <ChatIcon />
-            {order?.unread_count > 0 && (
-              <UreadMessageAlert>
-                <Dot />
-              </UreadMessageAlert>
-            )}
-          </ButtonLink>
-        )}
-        <ReactToPrint
-          trigger={() => (
+        <ButtonGroup>
+          {user?.level !== 5 && (
             <ButtonLink
               color='black'
+              active={openMessage?.chat}
+              onClick={() => handleOpenMessages('chat')}
               isDisabled={isTourOpen && currentTourStep === 1}
             >
-              <Printer />
+              <ChatIcon />
+              {order?.unread_count > 0 && (
+                <UreadMessageAlert>
+                  <Dot />
+                </UreadMessageAlert>
+              )}
             </ButtonLink>
           )}
-          content={() => printRef.current}
-          removeAfterPrint
-        />
-        <ButtonLink
-          color='black'
-          active={openMessage?.history}
-          onClick={() => handleOpenMessages('history')}
-          isDisabled={isTourOpen && currentTourStep === 1}
-        >
-          <Diagram3 />
-        </ButtonLink>
-        <ButtonLink
-          color='black'
-          active={showOption === 'metafields'}
-          onClick={() => handleShowOption('metafields')}
-          isDisabled={isTourOpen && currentTourStep === 1}
-        >
-          <ThreeDots />
-        </ButtonLink>
-        <ButtonLink
-          color='black'
-          onClick={() => closeSideBar()}
-        >
-          <CloseIcon />
-        </ButtonLink>
-      </ButtonGroup>
+          <ReactToPrint
+            trigger={() => (
+              <ButtonLink
+                color='black'
+                isDisabled={isTourOpen && currentTourStep === 1}
+              >
+                <Printer />
+              </ButtonLink>
+            )}
+            content={() => printRef.current}
+            removeAfterPrint
+          />
+          <ButtonLink
+            color='black'
+            active={openMessage?.history}
+            onClick={() => handleOpenMessages('history')}
+            isDisabled={isTourOpen && currentTourStep === 1}
+          >
+            <Diagram3 />
+          </ButtonLink>
+          <ButtonLink
+            color='black'
+            active={showOption === 'metafields'}
+            onClick={() => handleShowOption('metafields')}
+            isDisabled={isTourOpen && currentTourStep === 1}
+          >
+            <ThreeDots />
+          </ButtonLink>
+          <ButtonLink
+            color='black'
+            onClick={() => closeSideBar()}
+          >
+            <CloseIcon />
+          </ButtonLink>
+        </ButtonGroup>
+      </div>
+      <p>
+        {order?.payment_events?.length > 0 ? (
+          order?.payment_events?.map((event, i) => (
+            <React.Fragment key={i}>
+              <span>
+                {event?.wallet_event
+                  ? walletName[event?.wallet_event?.wallet?.type]?.name
+                  : event?.paymethod?.name}
+              </span>
+              <EnDotSingle />
+            </React.Fragment>
+          ))
+        ) : (
+          <>
+            <span>{order?.paymethod?.name}</span>
+            {stripePaymethods.includes(order?.paymethod?.gateway) && (
+              <>
+                <span> (</span>
+                <StripeLink href={`https://dashboard.stripe.com/payments/${order?.pay_data}`} target='_blank'>{order?.pay_data}</StripeLink>
+                <span>) </span>
+                <span> ({order?.refund_data ? t('REFUNDED', 'Refunded') : t('MOBILE_SUCCESS', 'Success')}) </span>
+              </>
+            )}
+            <EnDotSingle />
+          </>
+        )}
+        {order?.delivery_type === 1 && (
+          <span>
+            {t('DELIVERY', 'Delivery')}
+          </span>
+        )}
+        {order?.delivery_type === 2 && (
+          <span>
+            {t('PICKUP', 'Pickup')}
+          </span>
+        )}
+        {order?.delivery_type === 3 && (
+          <span>
+            {t('EAT_IN', 'Eat in')}
+          </span>
+        )}
+        {order?.delivery_type === 4 && (
+          <span>
+            {t('CURBSIDE', 'Curbside')}
+          </span>
+        )}
+        {order?.delivery_type === 5 && (
+          <span>
+            {t('DRIVE_THRU', 'Drive thru')}
+          </span>
+        )}
+      </p>
     </OrderDetailsHeaderContainer>
   )
 }
