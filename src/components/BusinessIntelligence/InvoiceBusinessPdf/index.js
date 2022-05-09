589a9627353acae1bc4d984d4236c2d08bc8649d
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-05-10 02:27:14 +0800

    Revert "[update] image"
    This reverts commit 5b7a0a4631c9c01862d1068e34b4a03eb23d7d98.

--- src/components/BusinessIntelligence/InvoiceBusinessPdf/index.js
+++ src/components/BusinessIntelligence/InvoiceBusinessPdf/index.js
@@ -1,5 +1,5 @@
 import { useLanguage, useUtils } from 'ordering-components-admin'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Image } from 'react-bootstrap'
 import { useTheme } from 'styled-components'
 
@@ -119,14 +119,40 @@ export const InvoiceBusinessPdf = (props) => {
     return objectStatus && objectStatus
   }
 
+  const logoImage = theme?.images?.logos?.logotype
+  const [base64ImageString, setBase64ImageString] = useState(null)
+
+  const getBase64ImageFromUrl = async (imageUrl) => {
+    const response = await fetch(imageUrl)
+    const blob = await response.blob()
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.addEventListener('load', function () {
+        resolve(reader.result)
+      }, false)
+      reader.onerror = (error) => {
+        return reject(error)
+      }
+      reader.readAsDataURL(blob)
+    })
+  }
+
+  useEffect(() => {
+    getBase64ImageFromUrl(logoImage).then(base64 => {
+      setBase64ImageString(base64)
+    })
+  }, [])
+
   return (
     <div style={styles.root}>
-      <Image
-        src={theme?.images?.logos?.logotype}
-        fluid
-        loading='lazy'
-        style={styles.imageWrapper}
-      />
+      {base64ImageString && (
+        <Image
+          src={base64ImageString}
+          fluid
+          loading='lazy'
+          style={styles.imageWrapper}
+        />
+      )}
       <br />
       <span>{t('INVOICE_FOR', 'Invoice for')} <b>{exportInvoiceList?.invoice?.business?.name}</b></span><br />
       {
