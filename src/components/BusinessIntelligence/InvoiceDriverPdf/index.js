3e5a6eb6b2c970bcc868bf10d7e64ded81aa7284
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-05-10 00:47:58 +0800

    [update] invoice logo editable

--- src/components/BusinessIntelligence/InvoiceDriverPdf/index.js
+++ src/components/BusinessIntelligence/InvoiceDriverPdf/index.js
@@ -1,6 +1,7 @@
 import { useLanguage, useUtils } from 'ordering-components-admin'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Image } from 'react-bootstrap'
+import { useTheme } from 'styled-components'
 
 export const InvoiceDriverPdf = (props) => {
   const {
@@ -8,6 +9,8 @@ export const InvoiceDriverPdf = (props) => {
     getSubtotal,
     getTotal
   } = props
+
+  const theme = useTheme()
   const [, t] = useLanguage()
   const [{ parseDate, parsePrice }] = useUtils()
 
@@ -77,6 +80,9 @@ export const InvoiceDriverPdf = (props) => {
       whiteSpace: 'normal',
       fontSize: '14px',
       backgroundColor: '#eee'
+    },
+    imageWrapper: {
+      width: '150px'
     }
   }
 
@@ -111,14 +117,40 @@ export const InvoiceDriverPdf = (props) => {
     return objectStatus && objectStatus
   }
 
+  const logoImage = theme?.images?.logos?.logotype
+  const [base64ImageString, setBase64ImageString] = useState(null)
+
+  const getBase64ImageFromUrl = async (imageUrl) => {
+    const response = await fetch(imageUrl)
+    const blob = await response.blob()
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.addEventListener('load', function () {
+        resolve(reader.result)
+      }, false)
+      reader.onerror = (error) => {
+        return reject(error)
+      }
+      reader.readAsDataURL(blob)
+    })
+  }
+
+  useEffect(() => {
+    getBase64ImageFromUrl(logoImage).then(base64 => {
+      setBase64ImageString(base64)
+    })
+  }, [])
+
   return (
     <div style={styles.root}>
-      <Image
-        src='https://reactdemo.ordering.co/ac950c6a4d2521f00bfc442ebfa83f77.svg'
-        fluid
-        height='45px'
-        width='150px'
-      />
+      {base64ImageString && (
+        <Image
+          src={base64ImageString}
+          fluid
+          loading='lazy'
+          style={styles.imageWrapper}
+        />
+      )}
       <br />
       <span>{t('INVOICE_FOR', 'Invoice for')} <b>{exportInvoiceList?.invoice?.driver?.name}</b></span><br />
       {
@@ -132,7 +164,7 @@ export const InvoiceDriverPdf = (props) => {
       <table style={styles.table}>
         <thead>
           <tr style={styles.table.thead.trFirst}>
-            <th style={styles.table.thead.th} colspan='5'>{t('ORDERS', 'Orders')}</th>
+            <th style={styles.table.thead.th} colSpan='5'>{t('ORDERS', 'Orders')}</th>
           </tr>
           <tr style={styles.table.thead.trLast}>
             <th style={styles.table.thead.th}>{t('EXPORT_ORDER_NUMBER', 'Order number')}</th>
@@ -157,11 +189,11 @@ export const InvoiceDriverPdf = (props) => {
         </tbody>
         <tfoot style={styles.table.tfoot}>
           <tr style={styles.table.tfoot.tr}>
-            <td style={styles.table.tfoot.tr.td} colspan='4'>{t('SUBTOTAL', 'Subtotal')}</td>
+            <td style={styles.table.tfoot.tr.td} colSpan='4'>{t('SUBTOTAL', 'Subtotal')}</td>
             <td style={styles.table.tfoot.tr.td}>{parsePrice(exportInvoiceList?.invoice?.orders_subtotal)}</td>
           </tr>
           <tr style={styles.table.tfoot.tr}>
-            <td style={styles.table.tfoot.tr.td} colspan='4'>{t('TOTAL', 'Total')}</td>
+            <td style={styles.table.tfoot.tr.td} colSpan='4'>{t('TOTAL', 'Total')}</td>
             <td style={styles.table.tfoot.tr.td}>{parsePrice(exportInvoiceList?.invoice?.orders_total)}</td>
           </tr>
         </tfoot>
@@ -169,7 +201,7 @@ export const InvoiceDriverPdf = (props) => {
       <table style={styles.table}>
         <thead>
           <tr style={styles.table.thead.trLast}>
-            <th style={styles.table.thead.th} colspan='2'>{t('TOTALS', 'Totals')}</th>
+            <th style={styles.table.thead.th} colSpan='2'>{t('TOTALS', 'Totals')}</th>
           </tr>
         </thead>
         <tbody>
