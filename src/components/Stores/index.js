6a122c411103a53bfc1b3bb35a3b4b7582d09799
Carlos Gomez <carlos.g@ordering.co>
2022-05-16 17:25:42 -0500

    fix imports (#425)

--- src/components/Stores/index.js
+++ src/components/Stores/index.js
@@ -5,7 +5,22 @@ import { BusinessesListingHeader } from './BusinessesListingHeader'
 import { BusinessActiveStateFilter } from './BusinessActiveStateFilter'
 import { BusinessTypeFilter } from './BusinessTypeFilter'
 import { BusinessesList } from './BusinessesList'
+import { BusinessCustomFields } from './BusinessCustomFields'
+import { BusinessDeliveryPickupMore } from './BusinessDeliveryPickupMore'
+import { BusinessDetail } from './BusinessDetail'
 import { BusinessDetails } from './BusinessDetails'
+import { BusinessFrontLayout } from './BusinessFrontLayout'
+import { BusinessMenu } from './BusinessMenu'
+import { BusinessNotifications } from './BusinessNotifications'
+import { BusinessOrderingChannels } from './BusinessOrderingChannels'
+import { BusinessPaymentMethods } from './BusinessPaymentMethods'
+import { BusinessPreorderDetails } from './BusinessPreorderDetails'
+import { BusinessPromotionList } from './BusinessPromotionList'
+import { BusinessSchedule } from './BusinessSchedule'
+import { BusinessSupport } from './BusinessSupport'
+import { BusinessSummary } from './BusinessSummary'
+import { BusinessTaxAndFees } from './BusinessTaxAndFees'
+import { BusinessWebhooks } from './BusinessWebhooks'
 import { AddBusinessForm } from './AddBusinessForm'
 import { ImportersLateralBar } from './ImportersLateralBar'
 import { WizardBusiness } from './WizardBusiness'
@@ -18,7 +33,22 @@ export {
   BusinessActiveStateFilter,
   BusinessTypeFilter,
   BusinessesList,
+  BusinessCustomFields,
+  BusinessDeliveryPickupMore,
+  BusinessDetail,
   BusinessDetails,
+  BusinessFrontLayout,
+  BusinessMenu,
+  BusinessNotifications,
+  BusinessOrderingChannels,
+  BusinessPaymentMethods,
+  BusinessPreorderDetails,
+  BusinessPromotionList,
+  BusinessSchedule,
+  BusinessSupport,
+  BusinessSummary,
+  BusinessTaxAndFees,
+  BusinessWebhooks,
   AddBusinessForm,
   ImportersLateralBar,
   WizardBusiness,
