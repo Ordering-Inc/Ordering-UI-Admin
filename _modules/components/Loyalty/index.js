dfa263c0e73d341fb5f543c75d148744025dc78f
Jesus David <jesusdpch00@gmail.com>
2022-05-24 16:03:21 -0400

    build

--- _modules/components/Loyalty/index.js
+++ _modules/components/Loyalty/index.js
@@ -3,6 +3,30 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "PointsWallet", {
+  enumerable: true,
+  get: function get() {
+    return _PointsWallet.PointsWallet;
+  }
+});
+Object.defineProperty(exports, "PointsWalletBusinessDetail", {
+  enumerable: true,
+  get: function get() {
+    return _PointsWalletBusinessDetail.PointsWalletBusinessDetail;
+  }
+});
+Object.defineProperty(exports, "PointsWalletBusinessList", {
+  enumerable: true,
+  get: function get() {
+    return _PointsWalletBusinessList.PointsWalletBusinessList;
+  }
+});
+Object.defineProperty(exports, "PointsWalletLevels", {
+  enumerable: true,
+  get: function get() {
+    return _PointsWalletLevels.PointsWalletLevels;
+  }
+});
 Object.defineProperty(exports, "RewardsPrograms", {
   enumerable: true,
   get: function get() {
@@ -11,3 +35,11 @@ Object.defineProperty(exports, "RewardsPrograms", {
 });
 
 var _RewardsPrograms = require("./RewardsPrograms");
+
+var _PointsWallet = require("./PointsWallet");
+
+var _PointsWalletLevels = require("./PointsWalletLevels");
+
+var _PointsWalletBusinessDetail = require("./PointsWalletBusinessDetail");
+
+var _PointsWalletBusinessList = require("./PointsWalletBusinessList");
