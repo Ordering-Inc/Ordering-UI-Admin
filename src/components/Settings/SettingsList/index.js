2eb1df20e60c0ed13619996d7c5c4d8bc0293a07
MingXue <mingxue@ordering.co>
2022-05-28 02:48:26 +0800

    fix max platform settings issue

--- src/components/Settings/SettingsList/index.js
+++ src/components/Settings/SettingsList/index.js
@@ -64,7 +64,7 @@ export const SettingsListUI = (props) => {
           if (isNaN(Number(item?.value)) || Number(item?.value) < 0) {
             invalidMessageList.push(t('VALIDATION_MUST_BIGGER_ZERO', `${item?.name} must be bigger than zero`).replace('_attribute_', item?.name))
           }
-          if (item?.key === 'platform_fee_percentage' && Number(item?.value) >= 100) {
+          if (item?.key === 'platform_fee_percentage' && Number(item?.value) > 100) {
             invalidMessageList.push(t('VALIDATION_MUST_SMALLER_HUNDRED', `${item?.name} must be not bigger than 100`).replace('_attribute_', item?.name))
           }
           break
