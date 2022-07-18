e89394be015c0cc73462a7daa7b74d08f3235e45
carlosg-ordering <carlos.g@ordering.co>
2022-07-18 18:48:25 -0500

    build

--- _modules/components/Orders/OrdersTable/index.js
+++ _modules/components/Orders/OrdersTable/index.js
@@ -580,7 +580,7 @@ var OrdersTable = function OrdersTable(props) {
       className: "bold"
     }, t('INVOICE_ORDER_NO', 'Order No.'), " ", order === null || order === void 0 ? void 0 : order.id), (allowColumns === null || allowColumns === void 0 ? void 0 : allowColumns.dateTime) && /*#__PURE__*/_react.default.createElement("p", {
       className: "date"
-    }, order !== null && order !== void 0 && order.delivery_datetime_utc ? parseDate(order === null || order === void 0 ? void 0 : order.delivery_datetime_utc) : parseDate(order === null || order === void 0 ? void 0 : order.delivery_datetime, {
+    }, parseDate(order === null || order === void 0 ? void 0 : order.delivery_datetime, {
       utc: false
     }))))), (allowColumns === null || allowColumns === void 0 ? void 0 : allowColumns.status) && !isSelectedOrders && /*#__PURE__*/_react.default.createElement("td", {
       className: "statusInfo"
