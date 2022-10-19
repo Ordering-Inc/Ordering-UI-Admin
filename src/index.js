470f06409fa7bae18b5735ab051cb4b9d5151b7a
carlosg-ordering <68757729+carlosg-ordering@users.noreply.github.com>
2022-10-19 13:23:16 -0500

    [FEAT] export components (#624)

--- src/index.js
+++ src/index.js
@@ -356,12 +356,13 @@ import {
   CitySelector,
   ColumnAllowSettingPopover,
   Pagination,
-  ImageCrop
+  ImageCrop,
+  NotFoundSource
 } from './components/Shared'
 import { LogoutButton } from './components/LogoutButton'
 import { LanguageSelector } from './components/LanguageSelector'
 
-import { capitalize } from './utils'
+import { capitalize, verifyDecimals } from './utils'
 
 // contexts
 import { ThemeContext, ThemeProvider, useTheme } from './contexts/ThemeContext'
@@ -738,6 +739,7 @@ export {
   ColumnAllowSettingPopover,
   Pagination,
   ImageCrop,
+  NotFoundSource,
 
   capitalize,
 
