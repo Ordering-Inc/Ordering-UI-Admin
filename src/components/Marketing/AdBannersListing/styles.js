4dfb8bc6436a241cbd71b37758d5c2e6b523f90d
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-11-03 10:16:20 -0600

    [Feat] Ad banners (#640)
    * [Feat] ad banners
    
    * [Update] ad banners
    
    * import

---
+++ src/components/Marketing/AdBannersListing/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const AdBannersListingContainer = styled.div`
+  flex: 1;
+  padding: 20px;
+  box-sizing: border-box;
+  transition: all 0.5s;
+  overflow: hidden;
+`
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  h1 {
+    font-size: 20px;
+    font-weight: 700;
+    margin: 0;
+  }
+`
+export const BannerPositionsList = styled.div`
+`
