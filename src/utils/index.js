153ace4def371de9240ddcc2efc88a7536da4ba8
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-11-04 05:20:24 -0600

    [Add] login functionality with Email OTP

--- src/utils/index.js
+++ src/utils/index.js
@@ -252,6 +252,25 @@ export const ribbonValues = {
   shape: 'rectangle',
   enabled: false
 }
+/**
+ * Format seconds to hh:mm:ss
+ * @param {number} seconds
+ */
+export const formatSeconds = (seconds) => {
+  // Hours, minutes and seconds
+  var hrs = ~~(seconds / 3600)
+  var mins = ~~((seconds % 3600) / 60)
+  var secs = ~~seconds % 60
+
+  // Output like '1:01' or '4:03:59' or '123:03:59'
+  var ret = ''
+  if (hrs > 0) {
+    ret += '' + hrs + ':' + (mins < 10 ? '0' : '')
+  }
+  ret += '' + mins + ':' + (secs < 10 ? '0' : '')
+  ret += '' + secs
+  return ret
+}
 
 /**
  * Function to check URL
