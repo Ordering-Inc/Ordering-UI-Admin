bbd11f78b8e06398b4f40a627ab5811cbd800fa6
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-11-05 01:07:03 -0600

    update

--- src/utils/index.js
+++ src/utils/index.js
@@ -258,12 +258,12 @@ export const ribbonValues = {
  */
 export const formatSeconds = (seconds) => {
   // Hours, minutes and seconds
-  var hrs = ~~(seconds / 3600)
-  var mins = ~~((seconds % 3600) / 60)
-  var secs = ~~seconds % 60
+  const hrs = Math.floor(seconds / 3600)
+  const mins = Math.floor((seconds % 3600) / 60)
+  const secs = Math.floor(seconds % 60)
 
   // Output like '1:01' or '4:03:59' or '123:03:59'
-  var ret = ''
+  let ret = ''
   if (hrs > 0) {
     ret += '' + hrs + ':' + (mins < 10 ? '0' : '')
   }
