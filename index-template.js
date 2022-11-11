7100cddf5715151103ffdb140ff5d84bbbeac89d
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-11-11 11:10:14 -0400

    [ADD] dashboard integration on settings (#660)

--- index-template.js
+++ index-template.js
@@ -34,7 +34,7 @@ import whatsappHeader from './template/assets/images/whatsapp-header.png'
 import appStore from './template/assets/images/app-store.png'
 import googlePlay from './template/assets/images/google-play.png'
 import deliverect from './template/assets/images/deliverect.png'
-
+import doordash from './template/assets/images/door-dash.png'
 /**
  * Theme apps
  */
@@ -172,7 +172,8 @@ theme.images = {
     whatsappHeader,
     appStore,
     googlePlay,
-    deliverect
+    deliverect,
+    doordash
   },
   apps: {
     storeApp,
