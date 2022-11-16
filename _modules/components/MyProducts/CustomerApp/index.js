eca78b469c941fedef42477a8bbe1ccc6ce0782b
DESKTOP-S4I8S56\Jesus <jesus@ordering.co>
2022-11-16 18:04:33 -0400

    build

--- _modules/components/MyProducts/CustomerApp/index.js
+++ _modules/components/MyProducts/CustomerApp/index.js
@@ -6,12 +6,8 @@ Object.defineProperty(exports, "__esModule", {
 exports.CustomerApp = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _orderingComponentsAdmin = require("ordering-components-admin");
-var _InfoShareContext = require("../../../contexts/InfoShareContext");
-var _styles = require("../../../styles");
-var _reactBootstrapIcons = require("react-bootstrap-icons");
 var _styledComponents = require("styled-components");
-var _styles2 = require("./styles");
-var _BoxLayout = require("../BoxLayout");
+var _AppLayout = require("../AppLayout");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -24,25 +20,22 @@ var CustomerApp = function CustomerApp() {
     _useLanguage2 = _slicedToArray(_useLanguage, 2),
     t = _useLanguage2[1];
   var theme = (0, _styledComponents.useTheme)();
-  var _useInfoShare = (0, _InfoShareContext.useInfoShare)(),
-    _useInfoShare2 = _slicedToArray(_useInfoShare, 2),
-    isCollapse = _useInfoShare2[0].isCollapse,
-    handleMenuCollapse = _useInfoShare2[1].handleMenuCollapse;
-  return /*#__PURE__*/_react.default.createElement(_styles2.Container, null, /*#__PURE__*/_react.default.createElement(_styles2.HeaderTitleContainer, null, isCollapse && /*#__PURE__*/_react.default.createElement(_styles.IconButton, {
-    color: "black",
-    onClick: function onClick() {
-      return handleMenuCollapse(false);
-    }
-  }, /*#__PURE__*/_react.default.createElement(_reactBootstrapIcons.List, null)), /*#__PURE__*/_react.default.createElement("h1", null, t('MY_PRODUCTS', 'My products'))), /*#__PURE__*/_react.default.createElement(_styles2.ContentWrapper, null, /*#__PURE__*/_react.default.createElement(_BoxLayout.BoxLayout, {
-    title: t('CUSTOMER_APP', 'Customer app'),
-    photo: theme.images.myProducts.customerApp,
-    description: t('CUSTOMER_APP_DESC', 'Give your customers the chance to order from their mobile devices wherever they are, receive push notifications, offers, and much more.')
-  }, /*#__PURE__*/_react.default.createElement(_styles2.ButtonWrapper, null, /*#__PURE__*/_react.default.createElement(_styles.Button, {
-    color: "primary",
-    borderRadius: "8px",
-    onClick: function onClick() {
-      return window.open('https://www.ordering.co/ordering-sales', '_blank');
-    }
-  }, t('REQUEST_APP_NOW', 'Request app now'))))));
+  var appInfo = {
+    title: t('CUSTOMER_APP', 'Customer App'),
+    description: t('CUSTOMER_APP_DESCRIPTION', 'Give your business owners or managers a tool to manage their whole business\'s incoming orders on the go by installing this app on any mobile device they have.'),
+    images: {
+      live: theme.images.myProducts.multiStoreCustomerApp,
+      brand: theme.images.myProducts.singleStoreCustomerApp
+    },
+    live_title: t('MULTI-STORE-CUSTOMER-APP', 'Multi-store Customer App'),
+    live_description: t('FREE_TO_USE_DESCRIPTION', 'This is a branded Ordering.co product. use it with your project, email, and password for free. features might be limited.'),
+    live_purchase_link: 'https://www.ordering.co/ordering-sales',
+    brand_title: t('SINGLE_STORE_CUSTOMER_APP', 'Single Store Customer App'),
+    brand_description: t('BRANDED_APP_DESCRIPTION', 'This App is delivered in less than five working days, fully branded with your guidelines, removing all ordering.co presence to give your brand more awareness.'),
+    purchase_link: 'https://www.ordering.co/ordering-sales'
+  };
+  return /*#__PURE__*/_react.default.createElement(_AppLayout.AppLayout, {
+    appInfo: appInfo
+  });
 };
 exports.CustomerApp = CustomerApp;
