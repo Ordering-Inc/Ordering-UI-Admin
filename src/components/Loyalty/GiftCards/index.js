8ccb6fb6a86314446a69d1eb63ad1027159ff28e
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-12-07 16:13:47 -0600

    [Feat] gift cards

---
+++ src/components/Loyalty/GiftCards/index.js
@@ -0,0 +1,198 @@
+import React, { useState, useEffect } from 'react'
+import { useLanguage, useUtils, PlatformProductsList as PlatformProductsListController } from 'ordering-components-admin'
+import { Alert, NotFoundSource, SideBar, Modal } from '../../Shared'
+// import { Switch } from '../../../styles'
+import { ChevronRight } from 'react-bootstrap-icons'
+import { useTheme } from 'styled-components'
+import Skeleton from 'react-loading-skeleton'
+import { useWindowSize } from '../../../hooks/useWindowSize'
+import { GiftCardDetail } from '../GiftCardDetail'
+
+import {
+  Container,
+  Title,
+  ProductContainer,
+  ItemContainer,
+  InfoWrapper,
+  WrapperImage,
+  ProductName,
+  Image,
+  AddNewButton,
+  BottomContainer,
+  NotFoundSourceWrapper,
+  PriceWrapper,
+  HeaderContainer,
+  RequireDescription
+} from './styles'
+
+const GiftCardsUI = (props) => {
+  const {
+    platformProductsListState,
+    handleParentSidebarMove,
+    handleSuccessDeleteProduct,
+    handleSuccessUpdateProduct,
+    handleSuccessAddProduct
+  } = props
+
+  const [, t] = useLanguage()
+  const theme = useTheme()
+  const [{ optimizeImage, parsePrice }] = useUtils()
+  const { width } = useWindowSize()
+
+  const [alertState, setAlertState] = useState({ open: false, content: [] })
+  const [extraOpen, setExtraOpen] = useState(false)
+  const [selectedProduct, setSelectedProduct] = useState(null)
+
+  const closeAlert = () => {
+    setAlertState({
+      open: false,
+      content: []
+    })
+  }
+
+  const handleOpenDetail = (level) => {
+    setSelectedProduct(level)
+    setExtraOpen(true)
+    if (width >= 1100) {
+      handleParentSidebarMove(550)
+    }
+  }
+
+  const handleGiftDetail = () => {
+    setExtraOpen(false)
+    setSelectedProduct(null)
+    handleParentSidebarMove(0)
+  }
+
+  useEffect(() => {
+    if (width < 1100) {
+      handleParentSidebarMove(0)
+    } else {
+      if (extraOpen) handleParentSidebarMove(550)
+    }
+  }, [width, extraOpen])
+
+  return (
+    <Container>
+      <HeaderContainer>
+        <Title>{t('GIFT_CARD', 'Gift Card')}</Title>
+        {/* <Switch
+          defaultChecked={review?.enabled}
+          onChange={val => handleChangeEnabled(val)}
+        /> */}
+      </HeaderContainer>
+      <RequireDescription>
+        <b>{t('', 'Stripe activation is required.')}</b> <i>{t('', 'Compatible only with Wallet.')}</i>
+      </RequireDescription>
+      <ProductContainer>
+        {platformProductsListState?.loading ? (
+          <>
+            {
+              [...Array(5).keys()].map(i => (
+                <ItemContainer key={i}>
+                  <InfoWrapper>
+                    <WrapperImage>
+                      <Skeleton className='d-flex h-100' />
+                    </WrapperImage>
+                    <ProductName>
+                      <Skeleton width={100} />
+                    </ProductName>
+                  </InfoWrapper>
+                  <Skeleton width={16} height={16} />
+                </ItemContainer>
+              ))
+            }
+          </>
+        ) : (
+          <>
+            {platformProductsListState?.products?.length > 0 ? platformProductsListState?.products.map(product => (
+              <ItemContainer
+                key={product.id}
+                onClick={() => handleOpenDetail(product)}
+                active={product?.id === selectedProduct?.id}
+              >
+                <InfoWrapper>
+                  <WrapperImage>
+                    <Image bgimage={optimizeImage(product?.image || theme.images?.dummies?.product)} />
+                  </WrapperImage>
+                  <ProductName>{product?.name}</ProductName>
+                </InfoWrapper>
+                <PriceWrapper>
+                  <span>{parsePrice(product?.price)}</span>
+                  <ChevronRight />
+                </PriceWrapper>
+              </ItemContainer>
+            )) : (
+              <NotFoundSourceWrapper>
+                <NotFoundSource />
+              </NotFoundSourceWrapper>
+            )}
+          </>
+        )}
+      </ProductContainer>
+      <BottomContainer>
+        <AddNewButton
+          onClick={() => handleOpenDetail({})}
+        >
+          {t('ADD_NEW_GIFT_CARD', 'Add new gift card')}
+        </AddNewButton>
+      </BottomContainer>
+      {extraOpen && (
+        <>
+          {width >= 1100 ? (
+            <SideBar
+              isBorderShow
+              open={extraOpen}
+              defaultSideBarWidth={550}
+              sidebarId='loyaltyLevel'
+              onClose={handleGiftDetail}
+            >
+              <GiftCardDetail
+                product={selectedProduct}
+                handleSuccessDeleteProduct={handleSuccessDeleteProduct}
+                handleSuccessUpdateProduct={handleSuccessUpdateProduct}
+                handleSuccessAddProduct={handleSuccessAddProduct}
+                setSelectedProduct={setSelectedProduct}
+                onClose={handleGiftDetail}
+              />
+            </SideBar>
+          ) : (
+            <Modal
+              width='70%'
+              height='90vh'
+              padding='0px'
+              open={extraOpen}
+              onClose={handleGiftDetail}
+            >
+              <GiftCardDetail
+                product={selectedProduct}
+                handleSuccessDeleteProduct={handleSuccessDeleteProduct}
+                handleSuccessUpdateProduct={handleSuccessUpdateProduct}
+                handleSuccessAddProduct={handleSuccessAddProduct}
+                setSelectedProduct={setSelectedProduct}
+                onClose={handleGiftDetail}
+              />
+            </Modal>
+          )}
+        </>
+      )}
+      <Alert
+        title={t('LEVELS', 'Levels')}
+        content={alertState.content}
+        acceptText={t('ACCEPT', 'Accept')}
+        open={alertState.open}
+        onClose={() => closeAlert()}
+        onAccept={() => closeAlert()}
+        closeOnBackdrop={false}
+      />
+    </Container>
+  )
+}
+
+export const GiftCards = (props) => {
+  const giftCardsProps = {
+    ...props,
+    UIComponent: GiftCardsUI
+  }
+  return <PlatformProductsListController {...giftCardsProps} />
+}
