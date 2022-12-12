e62ca94c5df221ae833d9c9bebeeab7750569ed3
Israel Rojas <israelrojas626@gmail.com>
2022-12-12 11:48:00 -0500

    [Update] sentry (#715)

--- index-template.js
+++ index-template.js
@@ -188,7 +188,60 @@ if (!(window?.location?.hostname === 'localhost')) {
       'is not a function',
       'can\'t find variable',
       'objects are not valid',
-      'element type is invalid'
+      'element type is invalid',
+      'TypeError: Failed to fetch',
+      'TypeError: NetworkError when attempting to fetch resource.',
+      'TypeError: Cancelled',
+      'TypeError: cancelado',
+      'is not defined',
+      "Can't find variable",
+      'NotAllowedError',
+      'SecurityError',
+      'Element type is invalid',
+      'undefined is not an object',
+      // Random plugins/extensions
+      'top.GLOBALS',
+      // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
+      'originalCreateNotification',
+      'canvas.contentDocument',
+      'MyApp_RemoveAllHighlights',
+      'http://tt.epicplay.com',
+      'Can\'t find variable: ZiteReader',
+      'jigsaw is not defined',
+      'ComboSearch is not defined',
+      'http://loading.retry.widdit.com/',
+      'atomicFindClose',
+      // Facebook borked
+      'fb_xd_fragment',
+      // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to reduce this. (thanks @acdha)
+      // See http://stackoverflow.com/questions/4113268/how-to-stop-javascript-injection-from-vodafone-proxy
+      'bmi_SafeAddOnload',
+      'EBCallBackMessageReceived',
+      // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+      'conduitPage',
+      // Generic error code from errors outside the security sandbox
+      // You can delete this if using raven.js > 1.0, which ignores these automatically.
+      'Script error.',
+      // Avast extension error
+      "_avast_submit"
+    ],
+    denyUrls: [
+      // Google Adsense
+      /pagead\/js/i,
+      // Facebook flakiness
+      /graph\.facebook\.com/i,
+      // Facebook blocked
+      /connect\.facebook\.net\/en_US\/all\.js/i,
+      // Woopra flakiness
+      /eatdifferent\.com\.woopra-ns\.com/i,
+      /static\.woopra\.com\/js\/woopra\.js/i,
+      // Chrome extensions
+      /extensions\//i,
+      /^chrome:\/\//i,
+      // Other plugins
+      /127\.0\.0\.1:4001\/isrunning/i,  // Cacaoweb
+      /webappstoolbarba\.texthelp\.com\//i,
+      /metrics\.itunes\.apple\.com\.edgesuite\.net\//i
     ],
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
