18277d8fc32e7567a2b0a13b47086cb32e89c71d
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-12-26 23:00:34 +0800

    [Improve] Appointments (#732)
    * [Add] specialist field
    
    * [Add] professionals info
    
    * [Add] info
    
    * [Update] professional schedule

--- src/components/Users/UsersListingHeader/index.js
+++ src/components/Users/UsersListingHeader/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { useLanguage } from 'ordering-components-admin'
 import { SearchBar } from '../../Shared'
-import { List as MenuIcon } from 'react-bootstrap-icons'
+import { List as MenuIcon, InfoCircle } from 'react-bootstrap-icons'
 import { IconButton } from '../../../styles'
 import { useInfoShare } from '../../../contexts/InfoShareContext'
 
 import {
   HeaderContainer,
   HeaderTitleContainer,
-  ActionContainer
+  ActionContainer,
+  InfoWrapper,
+  InfoContent
 } from './styles'
 
 export const UsersListingHeader = (props) => {
   const {
+    isProfessionals,
     title,
     searchValue,
     onSearch
@@ -33,6 +36,18 @@ export const UsersListingHeader = (props) => {
           </IconButton>
         )}
         <h1>{title}</h1>
+        {isProfessionals && (
+          <InfoWrapper>
+            <IconButton
+              color='primary'
+            >
+              <InfoCircle />
+            </IconButton>
+            <InfoContent>
+              {t('PROFESSIONALS_INFO', 'Create, assign and manage your professionals. Once the professional link his Google account, continue synchronizing your appointments with the professionals user using the Synchronize button inside of the account. Select the stores and configure the schedules for your services.')}
+            </InfoContent>
+          </InfoWrapper>
+        )}
       </HeaderTitleContainer>
       <ActionContainer>
         <SearchBar
