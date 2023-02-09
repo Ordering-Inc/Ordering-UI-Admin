fddf1994ef6479293f7e27a102634b3b68ed5c9b
carlosg-ordering <carlos.g@ordering.co>
2023-02-09 14:44:29 -0600

    build

--- _modules/components/MyProducts/AdvancedSettings/index.js
+++ _modules/components/MyProducts/AdvancedSettings/index.js
@@ -129,7 +129,7 @@ var AdvancedSettings = function AdvancedSettings(props) {
     type: 'hidden',
     path: 'business_listing_view.components.business.components.offer.hidden'
   }, {
-    name: t('OFFER__BADGE_POSITION', 'Offer badge position'),
+    name: t('OFFER_BADGE_POSITION', 'Offer badge position'),
     type: 'position',
     path: 'business_listing_view.components.business.components.offer.position'
   }, {
@@ -282,7 +282,7 @@ var AdvancedSettings = function AdvancedSettings(props) {
     type: 'hidden',
     path: 'profile.components.languages.hidden'
   }, {
-    name: t('address_list', 'Address list'),
+    name: t('ADDRESS_LIST', 'Address list'),
     type: 'hidden',
     path: 'profile.components.address_list.hidden'
   }];
--- _modules/components/MyProducts/CustomerApp/index.js
+++ _modules/components/MyProducts/CustomerApp/index.js
@@ -57,10 +57,10 @@ var CustomerAppUI = function CustomerAppUI(props) {
       brand: theme.images.myProducts.singleStoreCustomerApp
     },
     live_title: t('MULTI-STORE-CUSTOMER-APP', 'Multi-store Customer App'),
-    live_description: t('FREE_TO_USE_DESCRIPTION', 'This is a branded Ordering.co product. use it with your project, email, and password for free. features might be limited.'),
+    live_description: t('MULTI_STORE_CUSTOMER_APP_DESCRIPTION', 'This is a branded Ordering.co product. use it with your project, email, and password for free. features might be limited.'),
     demo_book_message: 'I would like to book a Demo for the customer app , can you assist me?',
     brand_title: t('SINGLE_STORE_CUSTOMER_APP', 'Single Store Customer App'),
-    brand_description: t('BRANDED_APP_DESCRIPTION', 'This App is delivered in less than five working days, fully branded with your guidelines, removing all ordering.co presence to give your brand more awareness.'),
+    brand_description: t('SINGLE_STORE_CUSTOMER_APP_DESCRIPTION', 'This App is delivered in less than five working days, fully branded with your guidelines, removing all ordering.co presence to give your brand more awareness.'),
     purchase_message: 'I would like to know about the customer app, can you assist me?',
     downloads: [{
       id: 1,
