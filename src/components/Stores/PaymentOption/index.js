e1d55b356958a26bbd005aa3aeb57b0798a83fac
Sky <59743717+Min-ordering@users.noreply.github.com>
2023-02-14 07:40:00 -0800

    [bug]: crash dashboard (#795)

--- src/components/Stores/PaymentOption/index.js
+++ src/components/Stores/PaymentOption/index.js
@@ -41,7 +41,7 @@ export const PaymentOption = (props) => {
   const [paymentTabs, setPaymentTabs] = useState(sitesState?.sites?.length > 0 ? 0 : 1)
   const [confirm, setConfirm] = useState({ open: false, content: null, handleOnAccept: null })
   const [localState, setLocalState] = useState({ allowed_order_types: businessPaymethod?.allowed_order_types, sites: businessPaymethod?.sites })
-  const filteredOptions = localState?.sites ?? businessPaymethod?.sites.filter(a => sitesState?.sites?.find(b => a.id === b.id))
+  const filteredOptions = localState?.sites ?? businessPaymethod?.sites?.filter(a => sitesState?.sites?.find(b => a.id === b.id))
   const [all, setAll] = useState(!!!filteredOptions?.length)
 
   const setPaymethodInfo = (values) => {
