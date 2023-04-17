94b2faf454d11a1b06de6c4f1e4eda45b313146c
Sky <59743717+Min-ordering@users.noreply.github.com>
2023-04-18 01:11:19 +0800

    [Update]: orders manager (#871)

--- src/components/Orders/OrdersContentHeader/index.js
+++ src/components/Orders/OrdersContentHeader/index.js
@@ -58,7 +58,8 @@ export const OrdersContentHeader = (props) => {
     } else {
       _filterApplied = filterValues?.groupTypes?.length || filterValues.businessIds.length > 0 || filterValues.cityIds.length > 0 ||
         filterValues.deliveryEndDatetime !== null || filterValues.deliveryFromDatetime !== null || filterValues.deliveryTypes.length > 0 ||
-        filterValues.driverIds.length > 0 || filterValues.paymethodIds.length > 0 || filterValues.statuses.length > 0 || filterValues?.metafield?.length > 0
+        filterValues.driverIds.length > 0 || filterValues.paymethodIds.length > 0 || filterValues.statuses.length > 0 || filterValues?.metafield?.length > 0 ||
+        filterValues?.externalId
     }
     setFilterApplied(_filterApplied)
   }, [filterValues])
