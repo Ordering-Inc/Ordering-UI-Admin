43e00823549ee955735e2f19283dcbc2f4771522
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-05-15 21:28:20 -0600

    [Update] website theme

---
+++ src/components/MyProducts/OrderingWebsite/SelectFranchise.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { useLanguage, useUtils } from 'ordering-components-admin'
+import { useTheme } from 'styled-components'
+import { DefaultSelect as Select } from '../../../styles'
+import { SelectWrapper, Option } from './styles'
+
+export const SelectFranchise = (props) => {
+  const {
+    franchisesList,
+    defaultValue,
+    onChange
+  } = props
+
+  const theme = useTheme()
+  const [, t] = useLanguage()
+  const [{ optimizeImage }] = useUtils()
+  const [options, setOptions] = useState([])
+
+  useEffect(() => {
+    if (franchisesList.loading) return
+    const _options = []
+    franchisesList.franchises.map(franchise => {
+      _options.push({
+        value: franchise?.id,
+        content: (
+          <Option>
+            <img src={optimizeImage(franchise?.logo || theme.images?.dummies?.businessLogo, 'h_50,c_limit')} alt='' />
+            <span>{franchise?.name}</span>
+          </Option>
+        )
+      })
+    })
+    setOptions(_options)
+  }, [franchisesList])
+
+  return (
+    <SelectWrapper>
+      <Select
+        placeholder={<Option>{t('SELECT_FRANCHISE', 'Select a franchise')}</Option>}
+        defaultValue={defaultValue}
+        options={options}
+        onChange={onChange}
+        optionInnerMaxHeight='300px'
+      />
+    </SelectWrapper>
+  )
+}
