160501f68b81159a35df838a0b26905dc6f3268d
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-05-29 23:33:54 +0800

    [Update] websocket status (#965)

--- src/components/Orders/WebsocketStatus/index.js
+++ src/components/Orders/WebsocketStatus/index.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import { useLanguage, useUtils, WebsocketStatus as WebsocketStatusController } from 'ordering-components-admin'
-import { Alert } from '../../Shared'
+import { InfoCircle } from 'react-bootstrap-icons'
+import { Alert, Modal } from '../../Shared'
+import { Button, IconButton } from '../../../styles'
+
 import {
   Contatiner,
   StatusContainer,
   WebsocketStatusDot,
-  DateWrapper
+  InfoWrapper,
+  InfoContent,
+  DetailContainer,
+  StatusItemWrapper,
+  ButtonsContainer
 } from './styles'
 
 const SocketStatusUI = (props) => {
   const {
     socketStatus,
-    getWebsocketStatus,
-    connectedDate,
-    reconnectAttemptCount
+    reconnectAttemptCount,
+    connectedDate
   } = props
 
   const [, t] = useLanguage()
   const [{ parseDate }] = useUtils()
   const [alertState, setAlertState] = useState({ open: false, content: [] })
+  const [openModal, setOpenModal] = useState(false)
 
   const closeAlert = () => {
     setAlertState({
@@ -39,15 +46,75 @@ const SocketStatusUI = (props) => {
 
   return (
     <Contatiner>
-      <StatusContainer>
-        <span>{getWebsocketStatus(socketStatus)}</span>
-        <WebsocketStatusDot
-          status={socketStatus}
-        />
-      </StatusContainer>
-      <DateWrapper>
-        {parseDate(connectedDate)}
-      </DateWrapper>
+      <InfoWrapper>
+        <IconButton
+          color='primary'
+        >
+          <InfoCircle />
+        </IconButton>
+        <InfoContent>
+          {t('WEBSOCKET_STATUS_INFO', 'Verify the server connection by date and time pressing the Connection status button Press update to refresh you dashboard and update the Connection status as well Need help? Contact our Customer support team here:')}
+          <a href='https://www.ordering.co/contact-ordering' target='_blank' rel='noopener noreferrer'>{t('CUSTOMER_SUPPORT', 'Customer support')}</a>
+        </InfoContent>
+      </InfoWrapper>
+      <Button
+        borderRadius='8px'
+        color='secundary'
+        onClick={() => setOpenModal(true)}
+      >
+        <StatusContainer>
+          <span>{t('CONNECTION_STATUS', 'Connection status')}</span>
+          <WebsocketStatusDot
+            status={socketStatus}
+          />
+        </StatusContainer>
+      </Button>
+
+      <Modal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        width='500px'
+      >
+        <DetailContainer>
+          <h2>{t('CONNECTION_STATUS', 'Connection status')}</h2>
+          <p>{t('LAST_UPDATE', 'Last update')}: {parseDate(connectedDate)}</p>
+          <StatusItemWrapper>
+            <div>
+              <WebsocketStatusDot status={1} />
+              <span>{t('OK', 'Ok')}</span>
+            </div>
+            <p>{t('WEBSOCKET_OK', 'The websocket works normally.')}</p>
+          </StatusItemWrapper>
+          <StatusItemWrapper>
+            <div>
+              <WebsocketStatusDot status={0} />
+              <span>{t('CONNECTING', 'Connecting')}</span>
+            </div>
+            <p>{t('WEBSOCKET_CONNECTING', 'The websocket is connecting.')}</p>
+          </StatusItemWrapper>
+          <StatusItemWrapper>
+            <div>
+              <WebsocketStatusDot status={2} />
+              <span>{t('DISCONNECTED', 'Disconnected')}</span>
+            </div>
+            <p>{t('WEBSOCKET_DISCONNECTED', 'The server is slow, please reload.')}</p>
+          </StatusItemWrapper>
+          <ButtonsContainer>
+            <Button
+              color='secundary'
+              onClick={() => setOpenModal(false)}
+            >
+              {t('CLOSE', 'Close')}
+            </Button>
+            <Button
+              color='primary'
+              onClick={() => window.location.reload()}
+            >
+              {t('UPDATE', 'Update')}
+            </Button>
+          </ButtonsContainer>
+        </DetailContainer>
+      </Modal>
 
       <Alert
         title={t('ERROR', 'Error')}
--- src/components/Orders/WebsocketStatus/styles.js
+++ src/components/Orders/WebsocketStatus/styles.js
@@ -1,10 +1,12 @@
 import styled, { css } from 'styled-components'
 
 export const Contatiner = styled.div`
-  background: ${props => props.theme.colors.secundary};
-  border-radius: 8px;
-  min-height: 41px;
-  padding: 0 15px;
+  display: flex;
+  align-items: center;
+
+  > button {
+    height: 41px;
+  }
   
   @media (min-width: 576px) {
     ${props => props.theme?.rtl ? css`
@@ -19,8 +21,14 @@ export const StatusContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  font-size: 14px;
-  font-weight: 700;
+
+  > span:first-child {
+    ${props => props.theme?.rtl ? css`
+      margin-left: 6px;
+    ` : css`
+      margin-right: 6px;
+    `}
+  }
 `
 
 export const WebsocketStatusDot = styled.span`
@@ -28,12 +36,6 @@ export const WebsocketStatusDot = styled.span`
   height: 8px;
   border-radius: 50%;
   display: inline-block;
-
-  ${props => props.theme?.rtl ? css`
-    margin-right: 6px;
-  ` : css`
-    margin-left: 6px;
-  `}
 
   ${({ status }) => status === 0 && css`
     background-color: #FF9922;
@@ -48,4 +50,122 @@ export const WebsocketStatusDot = styled.span`
 export const DateWrapper = styled.div`
   font-size: 12px;
   text-align: center;
+`
+export const InfoWrapper = styled.div`
+  position: relative;
+  ${props => props.theme?.rtl ? css`
+    margin-left: 4px;
+  ` : css`
+    margin-right: 4px;
+  `}
+
+  > button {
+    height: 25px;
+    svg {
+      width: 16px;
+      height: 16px;
+    }
+  }
+  &:hover > div {
+    visibility: visible;
+    opacity: 1;
+  }
+`
+
+export const InfoContent = styled.div`
+  position: absolute;
+  top: 100%;
+  z-index: 999;
+  background: ${props => props.theme.colors.backgroundInfo};
+  border: 1px solid ${props => props.theme.colors.primary};
+  box-sizing: border-box;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.12);
+  border-radius: 7.6px;
+  padding: 8px 12px;
+  font-size: 12px;
+  line-height: 18px;
+  min-width: 150px;
+  transition: all 0.3s linear;
+  visibility: hidden;
+  opacity: 0;
+  /* display: none; */
+  right: 0px;
+  ${props => props.theme.rtl && css`
+    left: 0px;
+    right: initial;
+  `}
+
+  a {
+    color: ${props => props.theme.colors.primary};
+    transition: all 0.3s linear;
+    cursor: pointer;
+    margin-left: 5px;
+    ${props => props.theme.rtl && css`
+      margin-left: 0px;
+      margin-right: 5px;
+    `}
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  @media (min-width: 576px) {
+    min-width: 350px;
+    padding: 12px 15px;
+    left: 0px;
+    ${props => props.theme.rtl && css`
+      right: 0px;
+      left: initial;
+    `}
+  }
+
+  @media (min-width: 993px) {
+    min-width: 535px;
+  }
+`
+export const DetailContainer = styled.div`
+  h2 {
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 30px;
+  }
+
+  > p {
+    font-size: 14px;
+  }
+`
+export const StatusItemWrapper = styled.div`
+  > div {
+    display: flex;
+    align-items: center;
+
+    > span:first-child {
+      margin: 0 16px;
+    }
+  }
+  p {
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 18px;
+    color: ${props => props.theme.colors.lightGray};
+
+    ${props => props.theme?.rtl ? css`
+      margin-right: 40px;
+    ` : css`
+      margin-left: 40px;
+    `}
+  }
+`
+export const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 40px;
+
+  > button {
+    border-radius: 8px;
+    height: 40px;
+    &:last-child {
+      margin: 0 10px;
+    }
+  }
 `
