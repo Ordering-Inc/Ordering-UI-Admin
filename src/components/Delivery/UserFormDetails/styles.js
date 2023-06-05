254d0526eb89b7b94b6a096ce690ca2e1a88f5f5
Sky <min@ordering.co>
2023-06-05 12:05:46 -0600

    [Update]: timezone for driver

--- src/components/Delivery/UserFormDetails/styles.js
+++ src/components/Delivery/UserFormDetails/styles.js
@@ -141,3 +141,30 @@ export const TogglePassword = styled.span`
     right: 10px;
   `}
 `
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 20px;
+
+  .select {
+    padding-top: 4px;
+    padding-bottom: 4px;
+    border: none;
+    background-color: ${props => props.theme.colors.secundary};
+    font-size: 14px;
+
+    ${({ isTimezone }) => isTimezone && css`
+      > div:first-child {
+        > div {
+          overflow: hidden;
+        }
+      }
+      .list-wrapper {
+        font-size: 12px;
+        max-height: 220px;
+      }
+    `}
+  }
+`
