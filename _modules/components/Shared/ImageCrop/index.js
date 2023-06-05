037b48f40ab53b05d7c018000f752a9007a38678
carlosg-ordering <carlos.g@ordering.co>
2023-06-05 16:45:36 -0600

    build

--- _modules/components/Shared/ImageCrop/index.js
+++ _modules/components/Shared/ImageCrop/index.js
@@ -14,6 +14,7 @@ var _styledComponents = require("styled-components");
 require("cropperjs/dist/cropper.css");
 var _styles2 = require("./styles");
 var _Checkbox = require("../../../styles/Checkbox");
+var _reactLoadingSkeleton = _interopRequireDefault(require("react-loading-skeleton"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -161,7 +162,11 @@ var ImageCrop = function ImageCrop(props) {
   var handleChangeAspectRatio = function handleChangeAspectRatio(checked) {
     checked ? cropper.setAspectRatio(18 / 9) : cropper.setAspectRatio(aspectRatio);
   };
-  return /*#__PURE__*/_react.default.createElement(_styles2.Container, null, /*#__PURE__*/_react.default.createElement(_styles2.ImageCropWrapper, null, /*#__PURE__*/_react.default.createElement("img", {
+  return /*#__PURE__*/_react.default.createElement(_styles2.Container, null, loading ? /*#__PURE__*/_react.default.createElement(_reactLoadingSkeleton.default, {
+    style: {
+      height: 400
+    }
+  }) : /*#__PURE__*/_react.default.createElement(_styles2.ImageCropWrapper, null, /*#__PURE__*/_react.default.createElement("img", {
     ref: cropperRef,
     src: photo
   })), /*#__PURE__*/_react.default.createElement(_styles2.ImageCropActionWrapper, null, /*#__PURE__*/_react.default.createElement(_styles2.RangeWrapper, null, /*#__PURE__*/_react.default.createElement("span", {
