8d42204f6f3c54f3045831c8b8f2f7205c9e9234
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-07-03 09:53:09 -0600

    [Add] delivery zones for selected business

--- src/components/Orders/CreateCustomOrder/Map/index.js
+++ src/components/Orders/CreateCustomOrder/Map/index.js
@@ -23,10 +23,27 @@ export const Map = (props) => {
     lng: !isInvalidDefaultLocation ? defaultLongitude : -73.9678064
   }
 
+  const greenFillStyle = {
+    fillColor: '#008000',
+    fillOpacity: 0.3,
+    strokeColor: '#008000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2
+  }
+
+  const units = {
+    mi: 1609,
+    km: 1000
+  }
+
   const defaultZoom = 10
   const [mapLoaded, setMapLoaded] = useState(true)
   const [mapCenter, setMapCenter] = useState(defaultCenter)
   const [mapZoom, setMapZoom] = useState(10)
+  const [map, setMap] = useState(null)
+  const [maps, setMaps] = useState(null)
+  const [circleZones, setCircleZones] = useState([])
+  const [polygonZones, setPolygonZones] = useState([])
 
   const handleMapChange = (data) => {
     if (!data?.zoom) return
@@ -51,6 +68,59 @@ export const Map = (props) => {
     newPoint = new window.google.maps.LatLng(marker.lat, marker.lng)
     bounds.extend(newPoint)
 
+    // drwa delivery zones
+    circleZones.forEach(circle => circle.setMap(null))
+    polygonZones.forEach(polygon => polygon.setMap(null))
+    const _circleZones = []
+    const _polygonZones = []
+    if (map && maps && business?.zones) {
+      const center = business?.location
+      for (const deliveryZone of business.zones) {
+        if (deliveryZone.type === 1 && deliveryZone?.data?.center && deliveryZone?.data?.radio) {
+          const newCircleZone = new window.google.maps.Circle({
+            ...greenFillStyle,
+            editable: false,
+            center: deliveryZone?.data.center,
+            radius: deliveryZone?.data.radio * 1000
+          })
+          newCircleZone.setMap(map)
+          bounds.union(newCircleZone.getBounds())
+          map.fitBounds(bounds)
+          _circleZones.push(newCircleZone)
+        }
+        if (deliveryZone.type === 5 && deliveryZone?.data?.distance) {
+          const newCircleZone = new window.google.maps.Circle({
+            ...greenFillStyle,
+            editable: false,
+            center: center,
+            radius: deliveryZone?.data.distance * units[deliveryZone?.data?.unit]
+          })
+          newCircleZone.setMap(map)
+          bounds.union(newCircleZone.getBounds())
+          map.fitBounds(bounds)
+          _circleZones.push(newCircleZone)
+        }
+        if (deliveryZone?.type === 2 && Array.isArray(deliveryZone?.data)) {
+          const newPolygonZone = new window.google.maps.Polygon({
+            ...greenFillStyle,
+            editable: false,
+            paths: deliveryZone?.data
+          })
+          newPolygonZone.setMap(map)
+          if (Array.isArray(deliveryZone?.data)) {
+            for (const position of deliveryZone?.data) {
+              bounds.extend(position)
+            }
+            map.fitBounds(bounds)
+          }
+          _polygonZones.push(newPolygonZone)
+        }
+      }
+    }
+    setCircleZones(_circleZones)
+    setPolygonZones(_polygonZones)
+
+    // fit map center and zoone
     const newBounds = {
       ne: {
         lat: bounds.getNorthEast()?.lat?.(),
@@ -74,7 +144,13 @@ export const Map = (props) => {
   useEffect(() => {
     if (mapLoaded) return
     mapFit()
-  }, [customerLocation, business?.location, mapLoaded])
+  }, [customerLocation, business?.location, mapLoaded, map, maps, business?.zones])
+
+  const onMapLoaded = (map, maps) => {
+    setMapLoaded(false)
+    setMap(map)
+    setMaps(maps)
+  }
 
   return (
     <Container ref={mapRef}>
@@ -84,7 +160,7 @@ export const Map = (props) => {
             key: window.document.getElementById('__googleMapsScriptId') ? null : googleMapsApiKey,
             libraries: ['places', 'geometry', 'drawing', 'visualization']
           }}
-          onGoogleApiLoaded={() => setMapLoaded(false)}
+          onGoogleApiLoaded={({ map, maps }) => onMapLoaded(map, maps)}
           defaultCenter={defaultCenter}
           center={mapCenter}
           defaultZoom={defaultZoom}
