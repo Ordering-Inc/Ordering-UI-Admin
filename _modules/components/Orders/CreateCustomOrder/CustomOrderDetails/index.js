b187518c14f75c35c2250d54a31eff3c0dacedb5
carlosg-ordering <carlos.g@ordering.co>
2023-07-07 09:29:25 -0600

    build

--- _modules/components/Orders/CreateCustomOrder/CustomOrderDetails/index.js
+++ _modules/components/Orders/CreateCustomOrder/CustomOrderDetails/index.js
@@ -14,7 +14,9 @@ var _Map = require("../Map");
 var _SelectProducts = require("../SelectProducts");
 var _Checkout = require("../Checkout");
 var _Shared = require("../../../Shared");
-var _styles = require("./styles");
+var _styles = require("../../../../styles");
+var _reactBootstrapIcons = require("react-bootstrap-icons");
+var _styles2 = require("./styles");
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
@@ -46,10 +48,17 @@ var CustomOrderDetailsUI = function CustomOrderDetailsUI(props) {
     cart = props.cart,
     onClose = props.onClose,
     handleOpenCustomOrderDetail = props.handleOpenCustomOrderDetail,
-    defaultCountryCodeState = props.defaultCountryCodeState;
+    defaultCountryCodeState = props.defaultCountryCodeState,
+    handlePlaceOrderByTotal = props.handlePlaceOrderByTotal,
+    extraFields = props.extraFields,
+    setExtraFields = props.setExtraFields,
+    actionState = props.actionState;
   var _useLanguage = (0, _orderingComponentsAdmin.useLanguage)(),
     _useLanguage2 = _slicedToArray(_useLanguage, 2),
     t = _useLanguage2[1];
+  var _useValidationFields = (0, _orderingComponentsAdmin.useValidationFields)(),
+    _useValidationFields2 = _slicedToArray(_useValidationFields, 1),
+    validationFields = _useValidationFields2[0];
   var _useOrder = (0, _orderingComponentsAdmin.useOrder)(),
     _useOrder2 = _slicedToArray(_useOrder, 2),
     orderState = _useOrder2[0],
@@ -61,6 +70,10 @@ var CustomOrderDetailsUI = function CustomOrderDetailsUI(props) {
     _useState2 = _slicedToArray(_useState, 2),
     alertState = _useState2[0],
     setAlertState = _useState2[1];
+  var _useState3 = (0, _react.useState)(true),
+    _useState4 = _slicedToArray(_useState3, 2),
+    isOrderByProducts = _useState4[0],
+    setIsOrderByProducts = _useState4[1];
   var handleCloseAlert = function handleCloseAlert() {
     setCustomersPhones(_objectSpread(_objectSpread({}, customersPhones), {}, {
       error: null
@@ -69,6 +82,27 @@ var CustomOrderDetailsUI = function CustomOrderDetailsUI(props) {
       open: false,
       content: []
     });
+  };
+  var handlePlaceOrder = function handlePlaceOrder() {
+    var _validationFields$fie, _validationFields$fie2, _validationFields$fie3, _validationFields$fie4, _validationFields$fie5, _validationFields$fie6;
+    var errMessage = [];
+    if (!selectedBusiness) {
+      errMessage.push(t('VALIDATION_ERROR_REQUIRED', 'Name is required').replace('_attribute_', t('BUSINESS', 'Business')));
+    }
+    if (!(extraFields !== null && extraFields !== void 0 && extraFields.total)) {
+      errMessage.push(t('VALIDATION_ERROR_REQUIRED', 'Name is required').replace('_attribute_', t('TOTAL', 'Total')));
+    }
+    if (validationFields !== null && validationFields !== void 0 && (_validationFields$fie = validationFields.fields) !== null && _validationFields$fie !== void 0 && (_validationFields$fie2 = _validationFields$fie.checkout) !== null && _validationFields$fie2 !== void 0 && (_validationFields$fie3 = _validationFields$fie2.comments) !== null && _validationFields$fie3 !== void 0 && _validationFields$fie3.enabled && validationFields !== null && validationFields !== void 0 && (_validationFields$fie4 = validationFields.fields) !== null && _validationFields$fie4 !== void 0 && (_validationFields$fie5 = _validationFields$fie4.checkout) !== null && _validationFields$fie5 !== void 0 && (_validationFields$fie6 = _validationFields$fie5.comments) !== null && _validationFields$fie6 !== void 0 && _validationFields$fie6.required && !(extraFields !== null && extraFields !== void 0 && extraFields.comment)) {
+      errMessage.push(t('VALIDATION_ERROR_REQUIRED', 'Name is required').replace('_attribute_', t('COMMENT', 'Comment')));
+    }
+    if (errMessage.length > 0) {
+      setAlertState({
+        open: true,
+        content: errMessage
+      });
+      return;
+    }
+    handlePlaceOrderByTotal();
   };
   var customerAddress = (0, _react.useMemo)(function () {
     var address = null;
@@ -95,7 +129,22 @@ var CustomOrderDetailsUI = function CustomOrderDetailsUI(props) {
       });
     }
   }, [customersPhones === null || customersPhones === void 0 ? void 0 : customersPhones.error]);
-  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_styles.DetailsContainer, {
+  (0, _react.useEffect)(function () {
+    if (actionState !== null && actionState !== void 0 && actionState.error) {
+      setAlertState({
+        open: true,
+        content: actionState === null || actionState === void 0 ? void 0 : actionState.error
+      });
+    }
+  }, [actionState]);
+  (0, _react.useEffect)(function () {
+    if (!isOrderByProducts) {
+      setExtraFields({
+        external_id: extraFields === null || extraFields === void 0 ? void 0 : extraFields.external_id
+      });
+    }
+  }, [isOrderByProducts]);
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_styles2.DetailsContainer, {
     className: "custom-order-content"
   }, /*#__PURE__*/_react.default.createElement("h2", null, t('CREATE_CUSTOM_ORDER', 'Create custom order')), /*#__PURE__*/_react.default.createElement(_SelectCustomer.SelectCustomer, {
     phone: phone,
@@ -114,19 +163,65 @@ var CustomOrderDetailsUI = function CustomOrderDetailsUI(props) {
     customer: selectedUser,
     customerLocation: customerAddress === null || customerAddress === void 0 ? void 0 : customerAddress.location,
     business: selectedBusiness
-  }), selectedBusiness && /*#__PURE__*/_react.default.createElement(_SelectProducts.SelectProducts, {
+  }), /*#__PURE__*/_react.default.createElement(_styles2.ToggleOptions, null, /*#__PURE__*/_react.default.createElement(_styles2.RaidoButton, {
+    onClick: function onClick() {
+      return setIsOrderByProducts(true);
+    }
+  }, isOrderByProducts ? /*#__PURE__*/_react.default.createElement(_reactBootstrapIcons.RecordCircleFill, {
+    className: "active"
+  }) : /*#__PURE__*/_react.default.createElement(_reactBootstrapIcons.Circle, null), /*#__PURE__*/_react.default.createElement("span", null, t('ORDER_BY_PRODUCTS', 'Order by products'))), /*#__PURE__*/_react.default.createElement(_styles2.RaidoButton, {
+    onClick: function onClick() {
+      return setIsOrderByProducts(false);
+    }
+  }, isOrderByProducts ? /*#__PURE__*/_react.default.createElement(_reactBootstrapIcons.Circle, null) : /*#__PURE__*/_react.default.createElement(_reactBootstrapIcons.RecordCircleFill, {
+    className: "active"
+  }), /*#__PURE__*/_react.default.createElement("span", null, t('ORDER_BY_TOTAL', 'Order by total')))), !isOrderByProducts && /*#__PURE__*/_react.default.createElement(_styles2.FormControl, null, /*#__PURE__*/_react.default.createElement("label", null, t('TOTAL', 'Total')), /*#__PURE__*/_react.default.createElement(_styles.Input, {
+    value: (extraFields === null || extraFields === void 0 ? void 0 : extraFields.total) || '',
+    onKeyPress: function onKeyPress(e) {
+      if (!/^[0-9]$/.test(e.key)) {
+        e.preventDefault();
+      }
+    },
+    onChange: function onChange(e) {
+      return setExtraFields(_objectSpread(_objectSpread({}, extraFields), {}, {
+        total: e.target.value
+      }));
+    },
+    placeholder: t('TOTAL', 'Total')
+  })), /*#__PURE__*/_react.default.createElement(_styles2.FormControl, null, /*#__PURE__*/_react.default.createElement("label", null, t('EXTERNAL_ID', 'External id'), /*#__PURE__*/_react.default.createElement("span", null, t('OPTIONAL', 'Optional'))), /*#__PURE__*/_react.default.createElement(_styles.Input, {
+    value: (extraFields === null || extraFields === void 0 ? void 0 : extraFields.external_id) || '',
+    onChange: function onChange(e) {
+      return setExtraFields(_objectSpread(_objectSpread({}, extraFields), {}, {
+        external_id: e.target.value
+      }));
+    },
+    placeholder: t('EXTERNAL_ID', 'External id')
+  })), !isOrderByProducts && /*#__PURE__*/_react.default.createElement(_styles2.FormControl, null, /*#__PURE__*/_react.default.createElement("label", null, t('COMMENT', 'Comment')), /*#__PURE__*/_react.default.createElement(_styles.TextArea, {
+    value: (extraFields === null || extraFields === void 0 ? void 0 : extraFields.comment) || '',
+    onChange: function onChange(e) {
+      return setExtraFields(_objectSpread(_objectSpread({}, extraFields), {}, {
+        comment: e.target.value
+      }));
+    },
+    placeholder: t('COMMENT', 'Comment')
+  })), selectedBusiness && isOrderByProducts && /*#__PURE__*/_react.default.createElement(_SelectProducts.SelectProducts, {
     productList: productList,
     getProducts: getProducts,
     handeUpdateProductCart: handeUpdateProductCart,
     cart: cart,
     business: selectedBusiness
-  }), cart && (cart === null || cart === void 0 ? void 0 : cart.products.length) > 0 && /*#__PURE__*/_react.default.createElement(_Checkout.Checkout, {
+  }), cart && (cart === null || cart === void 0 ? void 0 : cart.products.length) > 0 && isOrderByProducts && /*#__PURE__*/_react.default.createElement(_Checkout.Checkout, {
     cartUuid: cart.uuid,
     onPlaceOrderClick: function onPlaceOrderClick(data, paymethod, cart) {
       (cart === null || cart === void 0 ? void 0 : cart.uuid) && handleOpenCustomOrderDetail(cart.uuid);
       onClose();
-    }
-  })), /*#__PURE__*/_react.default.createElement(_Shared.Alert, {
+    },
+    extraFields: extraFields
+  }), !isOrderByProducts && /*#__PURE__*/_react.default.createElement(_styles2.ButtonWrapper, null, /*#__PURE__*/_react.default.createElement(_styles.Button, {
+    color: "primary",
+    onClick: handlePlaceOrder,
+    disabled: actionState === null || actionState === void 0 ? void 0 : actionState.loading
+  }, t('PLACE_ORDER', 'Place Order')))), /*#__PURE__*/_react.default.createElement(_Shared.Alert, {
     title: t('ERROR', 'Error'),
     open: alertState.open,
     content: alertState.content,
