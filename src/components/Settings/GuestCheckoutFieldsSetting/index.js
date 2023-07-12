5d51ebb57b5cd4e2437d830c0cba77aad5f9fdce
Sky <min@ordering.co>
2023-07-12 05:36:48 -0600

    Update: guest checkout setting

--- src/components/Settings/GuestCheckoutFieldsSetting/index.js
+++ src/components/Settings/GuestCheckoutFieldsSetting/index.js
@@ -1,13 +1,17 @@
 import React, { useState, useMemo } from 'react'
 import { useLanguage, useConfig, GuestCheckoutFieldsSetting as GuestCheckoutFieldsSettingController } from 'ordering-components-admin'
-import { DefaultSelect, Switch } from '../../../styles'
+import { DefaultSelect, IconButton, Switch } from '../../../styles'
 import Skeleton from 'react-loading-skeleton'
+import { InfoCircle } from 'react-bootstrap-icons'
 
 import {
   CheckoutFieldsDetailsContainer,
   FieldContainer,
   SelectWrapper,
-  Option
+  Option,
+  TitleWrapper,
+  InfoWrapper,
+  InfoContent
 } from './styles'
 
 const GuestCheckoutFieldsSettingUI = (props) => {
@@ -22,6 +26,7 @@ const GuestCheckoutFieldsSettingUI = (props) => {
   const [orderType, setOrderType] = useState('1')
 
   const hideSettingList = ['city_dropdown_option', 'address', 'address_notes']
+  const orderValidationFields = ['name', 'middle_name', 'lastname', 'second_lastname', 'email', 'mobile_phone', 'city_dropdown_option', 'address', 'zipcode', 'address_notes', 'coupon', 'driver_tip', 'comments', 'birthdate']
 
   const orderTypeList = useMemo(() => {
     const orderTypes = configs?.order_types_allowed?.options?.map(item => ({ value: item.value, content: <Option>{t(item.text, item.text?.replace('_', ' ').toLowerCase())}</Option> }))
@@ -31,7 +36,19 @@ const GuestCheckoutFieldsSettingUI = (props) => {
   return (
     <>
       <CheckoutFieldsDetailsContainer>
-        <h1>{t('CHECKOUT_CUSTOMER_FIELDS', 'Checkout and Customer fields')}</h1>
+        <TitleWrapper>
+          <h1>{t('CHECKOUT_CUSTOMER_FIELDS', 'Checkout and Customer fields')}</h1>
+          <InfoWrapper>
+            <IconButton
+              color='primary'
+            >
+              <InfoCircle />
+            </IconButton>
+            <InfoContent>
+              {t('GUEST_CHECKOUT_SETTING_INFO', 'Manage the fields that your guest users will be able to select and the fields required to complete an order (These options only apply for guest users)')}
+            </InfoContent>
+          </InfoWrapper>
+        </TitleWrapper>
         <SelectWrapper>
           <label>{t('FRONT_MAIN_EMAIL_ORDER_TYPE', 'Order type')}</label>
           <DefaultSelect
@@ -60,23 +77,30 @@ const GuestCheckoutFieldsSettingUI = (props) => {
           ))
         ) : (
           <>
-            {checkoutFieldsState.fields.filter(item => item?.order_type_id === parseInt(orderType)).map(field => !hideSettingList.includes(field?.validation_field.code) && (
-              <FieldContainer key={field.id}>
-                <div className='name'>{t(field?.validation_field.code.toUpperCase(), field?.validation_field.name)}</div>
-                <div className='required'>
-                  <Switch
-                    defaultChecked={field?.required}
-                    onChange={(val) => handleChangeCheckoutFieldSetting(field?.validation_field_id, field?.order_type_id, { required: val })}
-                  />
-                </div>
-                <div className='status'>
-                  <Switch
-                    defaultChecked={field?.enabled}
-                    onChange={(val) => handleChangeCheckoutFieldSetting(field?.validation_field_id, field?.order_type_id, { enabled: val })}
-                  />
-                </div>
-              </FieldContainer>
-            ))}
+            {checkoutFieldsState.fields
+              .filter(item => item?.order_type_id === parseInt(orderType))
+              .sort((a, b) => {
+                const indexA = orderValidationFields.indexOf(a?.validation_field.code)
+                const indexB = orderValidationFields.indexOf(b?.validation_field.code)
+                return indexA - indexB
+              })
+              .map(field => !hideSettingList.includes(field?.validation_field.code) && !([2, 3, 4, 5, 6].includes(field?.order_type_id) && field?.validation_field.code === 'driver_tip') && (
+                <FieldContainer key={field.id}>
+                  <div className='name'>{t(field?.validation_field.code.toUpperCase(), field?.validation_field.name)}</div>
+                  <div className='required'>
+                    <Switch
+                      defaultChecked={field?.required}
+                      onChange={(val) => handleChangeCheckoutFieldSetting(field?.validation_field_id, field?.order_type_id, { required: val })}
+                    />
+                  </div>
+                  <div className='status'>
+                    <Switch
+                      defaultChecked={field?.enabled}
+                      onChange={(val) => handleChangeCheckoutFieldSetting(field?.validation_field_id, field?.order_type_id, { enabled: val })}
+                    />
+                  </div>
+                </FieldContainer>
+              ))}
           </>
         )}
       </CheckoutFieldsDetailsContainer>
--- src/components/Settings/GuestCheckoutFieldsSetting/styles.js
+++ src/components/Settings/GuestCheckoutFieldsSetting/styles.js
@@ -6,17 +6,6 @@ export const CheckoutFieldsDetailsContainer = styled.div`
   width: 100%;
   overflow-x: hidden;
 
-  h1 {
-    font-weight: 700;
-    font-size: 20px;
-    margin-bottom: 10px;
-    ${props => props.theme?.rtl ? css`
-      margin-left: 40px;
-    ` : css`
-      margin-right: 40px;
-    `}
-  }
-
   @media (min-width: 768px) {
     padding: 26px 40px;
   }
@@ -102,5 +91,74 @@ export const FieldContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+`
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  ${props => props.theme?.rtl ? css`
+    margin-left: 40px;
+  ` : css`
+    margin-right: 40px;
+  `}
+
+  h1 {
+    font-weight: 700;
+    font-size: 20px;
+    margin-bottom: 0;
+  }
+`
+
+export const InfoWrapper = styled.div`
+  position: relative;
+  ${props => props.theme?.rtl ? css`
+    margin-left: -8px;
+    margin-right: 8px;
+  ` : css`
+    margin-right: -8px;
+    margin-left: 8px;
+  `}
+
+  > button {
+    height: 25px;
+    svg {
+      width: 16px;
+      height: 16px;
+    }
+  }
+  &:hover > div {
+    /* display: block; */
+    visibility: visible;
+    opacity: 1;
+  }
+`
+
+export const InfoContent = styled.div`
+  position: absolute;
+  top: 100%;
+  z-index: 999;
+  background: ${props => props.theme.colors.backgroundInfo};
+  border: 1px solid ${props => props.theme.colors.primary};
+  box-sizing: border-box;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.12);
+  border-radius: 7.6px;
+  padding: 8px 12px;
+  font-size: 12px;
+  line-height: 18px;
+  min-width: 150px;
+  transition: all 0.3s linear;
+  visibility: hidden;
+  opacity: 0;
+  right: 0px;
+  ${props => props.theme.rtl && css`
+    left: 0px;
+    right: initial;
+  `}
+
+  @media (min-width: 576px) {
+    min-width: 330px;
+    padding: 12px 15px;
   }
 `
