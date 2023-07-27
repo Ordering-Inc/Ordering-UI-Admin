f34fa0eef777263404a4d27cc457e30da349e87f
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-07-26 21:08:41 -0600

    [Update] Recovery actions

--- src/components/Marketing/CampaignListing/index.js
+++ src/components/Marketing/CampaignListing/index.js
@@ -4,8 +4,7 @@ import { SideBar } from '../../Shared'
 import { CampaignDetail } from '../CampaignDetail'
 import { CampaignHeader } from '../CampaignHeader'
 import { CampaignList } from '../CampaignList'
-// import { CampaignListing as CampaignListingController } from 'ordering-components-admin'
-import { CampaignListing as CampaignListingController } from './naked'
+import { CampaignListing as CampaignListingController } from 'ordering-components-admin'
 import { addQueryToUrl, removeQueryToUrl } from '../../../utils'
 
 import {
--- template/pages/RecoveryActionListing/index.js
+++ template/pages/RecoveryActionListing/index.js
@@ -3,10 +3,14 @@ import { HelmetTags } from '../../components/HelmetTags'
 import { RecoveryActionListing as RecoveryActionListingController } from '../../../src/components/CartRecovery'
 
 export const RecoveryActionListing = (props) => {
+  const recoveryActionListProps = {
+    ...props,
+    isUseQuery: true
+  }
   return (
     <>
       <HelmetTags page='recovery_actions' />
-      <RecoveryActionListingController {...props} />
+      <RecoveryActionListingController {...recoveryActionListProps} />
     </>
   )
 }
