2d1d3e3d563d8d8f662fdcaff84658b206973b1a
Sky <59743717+Min-ordering@users.noreply.github.com>
2023-07-27 11:30:22 -0600

    [Update]: report in analytics (#1074)
    (cherry picked from commit 242339ebbeb537caeb2add6a9bb31f09a52cc37b)

--- src/components/BusinessIntelligence/AnalyticsOrdersOrSales/index.js
+++ src/components/BusinessIntelligence/AnalyticsOrdersOrSales/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
 import 'chartjs-adapter-moment'
-import moment from 'moment'
 import {
   Container,
   ChartContentWrapper,
@@ -28,127 +27,10 @@ export const AnalyticsOrdersOrSales = (props) => {
   const [{ parsePrice }] = useUtils()
   const chartRef = useRef(null)
   const [isShowPreview, setIsShowPreview] = useState(false)
-  const [timeAxes, setTimeAxes] = useState([])
-
-  const getTimeAxes = (lapse) => {
-    const xAxes = {
-      type: 'time',
-      grid: {
-        display: false
-      },
-      time: {
-        stepSize: 1,
-        displayFormats: {
-          hour: 'LT'
-        }
-      },
-      ticks: {
-        fontSize: 12,
-        fontColor: '#CED4DA'
-      }
-    }
-    switch (lapse) {
-      case 'today':
-      case 'yesterday':
-        xAxes.time.unit = 'hour'
-        xAxes.time.min = moment().subtract(lapse === 'today' ? 0 : 1, 'days').format('YYYY-MM-DD 00:00:00')
-        xAxes.time.max = moment().subtract(lapse === 'today' ? 0 : 1, 'days').format('YYYY-MM-DD 24:00:00')
-        break
-      case 'last_7_days':
-      case 'last_30_days':
-        xAxes.time.unit = 'day'
-        xAxes.time.min = moment().subtract(lapse === 'last_7_days' ? 7 : 30, 'days').format('YYYY-MM-DD')
-        xAxes.time.max = moment().format('YYYY-MM-DD')
-        break
-      default: {
-        const _lapse = lapse.split(',')
-        const from = moment(_lapse[0] + ' 00:00:00')
-        const to = moment(_lapse[1] + ' 24:00:00')
-        const duration = moment.duration(from.diff(to))
-        const hours = Math.abs(duration.asHours())
-        const days = Math.abs(duration.asDays())
-        const months = Math.abs(duration.asMonths())
-        if (hours <= 24) {
-          xAxes.time.unit = 'hour'
-          xAxes.time.min = from.format('YYYY-MM-DD HH:mm:ss')
-          xAxes.time.max = to.format('YYYY-MM-DD HH:mm:ss')
-        } else if (days <= 30) {
-          xAxes.time.unit = 'day'
-          xAxes.time.min = from.format('YYYY-MM-DD HH:mm:ss')
-          xAxes.time.max = to.format('YYYY-MM-DD HH:mm:ss')
-        } else if (months <= 12) {
-          xAxes.time.unit = 'month'
-          xAxes.time.min = from.format('YYYY-MM-DD HH:mm:ss')
-          xAxes.time.max = to.format('YYYY-MM-DD HH:mm:ss')
-        } else {
-          xAxes.time.unit = 'year'
-          xAxes.time.min = from.format('YYYY-MM-DD HH:mm:ss')
-          xAxes.time.max = to.format('YYYY-MM-DD HH:mm:ss')
-        }
-        break
-      }
-    }
-    return xAxes
-  }
-
-  const updateTimeAxes = () => {
-    var unitdate = getTimeAxes(filterList?.lapse).time.unit
-    var maxdate = moment(getTimeAxes(filterList?.lapse).time.max).endOf(unitdate)
-    var mindate = moment(getTimeAxes(filterList?.lapse).time.min).startOf(unitdate)
-    var curDate = mindate
-    var newTimeAxes = []
-    var adder = 'd'
-    switch (unitdate) {
-      case 'day': adder = 'd'
-        break
-      case 'month': adder = 'M'
-        break
-      case 'year': adder = 'y'
-        break
-      case 'hour': adder = 'h'
-        break
-      default: break
-    }
-    do {
-      newTimeAxes.push(moment(curDate._d.getTime()).format('YYYY-MM-DD HH:mm:ss'))
-      curDate = curDate.clone().add(1, adder)
-    }
-    while (curDate <= maxdate)
-    setTimeAxes(newTimeAxes)
-  }
-
-  const generateData = () => {
-    const values = []
-    timeAxes.forEach(function (axe) {
-      const index = chartDataList?.data?.findIndex(history => {
-        return axe === history?.time
-      })
-      if (index !== -1) {
-        values.push({ x: chartDataList?.data[index]?.time, y: (isOrders ? chartDataList?.data[index]?.orders : chartDataList?.data[index]?.sales) })
-      } else {
-        values.push({ x: axe, y: 0 })
-      }
-    })
-    return values
-  }
-
-  const defaultData = {
-    datasets: [
-      {
-        label: isOrders ? t('ORDERS', 'Orders') : t('SALES', 'Sales'),
-        data: generateData(),
-        fill: false,
-        backgroundColor: 'rgba(75,192,192,0.2)',
-        borderColor: isOrders ? '#2C7BE5' : '#27BCFD',
-        tension: 0.4,
-        borderWidth: 3
-      }
-    ]
-  }
+  const [dataOptions, setDataOptions] = useState(null)
 
   const options = {
     scales: {
-      x: getTimeAxes(filterList?.lapse),
       y: {
         beginAtZero: true,
         grid: {
@@ -216,9 +98,37 @@ export const AnalyticsOrdersOrSales = (props) => {
     setIsShowPreview(true)
   }
 
+  const generateLabel = () => {
+    const values = []
+    chartDataList?.data && chartDataList.data.forEach(data => {
+      values.push(data.time)
+    })
+    return values
+  }
+
+  const generateData = () => {
+    const values = chartDataList.data.map(item => isOrders ? item.orders : item.sales)
+
+    return [{
+      label: isOrders ? t('ORDERS', 'Orders') : t('SALES', 'Sales'),
+      data: [...values],
+      fill: false,
+      backgroundColor: 'rgba(75,192,192,0.2)',
+      borderColor: '#2C7BE5',
+      tension: 0.4,
+      borderWidth: 3
+    }]
+  }
+
   useEffect(() => {
-    updateTimeAxes()
-  }, [filterList])
+    if (chartDataList?.data && chartDataList?.data?.length > 0) {
+      const defaultData = {
+        labels: generateLabel(),
+        datasets: generateData()
+      }
+      setDataOptions(defaultData)
+    }
+  }, [chartDataList])
 
   return (
     <>
@@ -235,7 +145,7 @@ export const AnalyticsOrdersOrSales = (props) => {
             chartDataList?.loading ? (
               <Skeleton height={150} />
             ) : (
-              chartDataList?.data.length > 0 ? <Line data={defaultData} options={options} ref={chartRef} /> : <EmptyContent>{t('NO_DATA', 'No Data')}</EmptyContent>
+              (chartDataList?.data.length > 0 && dataOptions) ? <Line data={dataOptions} options={options} ref={chartRef} /> : <EmptyContent>{t('NO_DATA', 'No Data')}</EmptyContent>
             )
           }
         </ChartContentWrapper>
@@ -257,7 +167,7 @@ export const AnalyticsOrdersOrSales = (props) => {
         onClose={() => setIsShowPreview(false)}
       >
         <ChartContentWrapper>
-          <Line data={defaultData} options={options} />
+          <Line data={dataOptions} options={options} />
         </ChartContentWrapper>
       </Modal>
     </>
