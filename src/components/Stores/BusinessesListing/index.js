98ebf0284c8c670446a63e444b5b19e8d92ffb60
Sky <59743717+Min-ordering@users.noreply.github.com>
2023-08-21 23:35:25 +0800

    Update: sentry feedback (#1122)

--- src/components/Stores/BusinessesListing/index.js
+++ src/components/Stores/BusinessesListing/index.js
@@ -83,7 +83,7 @@ const BusinessesListingUI = (props) => {
       businessList?.businesses?.length === 0 &&
       pagination?.currentPage === 1 &&
       !searchValue &&
-      Object.keys(filterValues).length === 0 &&
+      (!filterValues || Object.keys(filterValues).length === 0) &&
       selectedBusinessActiveState &&
       !businessTypeSelected &&
       inActiveBusinesses?.length === 0
