b24222fa7df9850c5af8651f5d8f3039cf9ea1dc
dreamsky2020 <jintie@ordering.co>
2023-09-19 10:56:25 -0400

    Remove live demo block

--- src/components/MyProducts/AppLayout/index.js
+++ src/components/MyProducts/AppLayout/index.js
@@ -24,6 +24,87 @@ export const AppLayout = (props) => {
   const [{ isCollapse }, { handleMenuCollapse }] = useInfoShare()
   const location = useLocation()
 
+  const LeftContent = () => (
+    <AppInfoContainer>
+      <ImageWrapper isDriver={isDriver}>
+        <img src={appInfo.images.live} alt='' />
+      </ImageWrapper>
+      <h2>{appInfo.live_title}</h2>
+      {appInfo?.downloads && (
+        <DownLoadWrapper>
+          {appInfo.downloads.map(item => (
+            <img
+              src={item.icon}
+              alt=''
+              key={item.id}
+              onClick={() => window.open(item.link, '_blank')}
+            />
+          ))}
+        </DownLoadWrapper>
+      )}
+      {appInfo?.web_url && (
+        <DownloadLink href={appInfo.web_url} target='_blank' isSingle className='download-link'>
+          {appInfo.web_link_title}
+        </DownloadLink>
+      )}
+      <p>{appInfo.live_description}</p>
+      {appInfo?.purchase_message && (
+        <ButtonWRapper>
+          {appInfo?.purchase_link && (
+            <Button
+              color='primary'
+              borderRadius='8px'
+              onClick={() => window.open(appInfo?.purchase_link, '_blank')}
+            >
+              {t('PURCHASE_NOW', 'Purchase Now')}
+            </Button>
+          )}
+          {appInfo?.demo_link && (
+            <Button
+              color='primary'
+              outline
+              borderRadius='8px'
+              onClick={() => window.open(appInfo?.demo_link, '_blank')}
+            >
+              {t('BOOK_A_DEMO', 'Book a demo')}
+            </Button>
+          )}
+        </ButtonWRapper>
+      )}
+    </AppInfoContainer>
+  )
+
+  const RightContent = () => (
+    <AppInfoContainer>
+      <ImageWrapper isDriver={isDriver}>
+        <img src={appInfo.images.brand} alt='' />
+      </ImageWrapper>
+      <h2>{appInfo.brand_title}</h2>
+      <p>{appInfo.brand_description}</p>
+      <ButtonWRapper>
+        {appInfo?.purchase_link && (
+          <Button
+            color='primary'
+            borderRadius='8px'
+            onClick={() => window.open(appInfo?.purchase_link, '_blank')}
+          >
+            {t('PURCHASE_NOW', 'Purchase Now')}
+          </Button>
+        )}
+        {!appInfo?.purchase_link && appInfo?.demo_link && (
+          <Button
+            color='primary'
+            outline
+            borderRadius='8px'
+            onClick={() => window.open(appInfo?.demo_link, '_blank')}
+          >
+            {t('BOOK_A_DEMO', 'Book a demo')}
+          </Button>
+        )}
+      </ButtonWRapper>
+    </AppInfoContainer>
+  )
+
   return (
     <Container>
       <HeaderTitleContainer>
@@ -54,67 +135,8 @@ export const AppLayout = (props) => {
         <h1>{appInfo.title}</h1>
         <p>{appInfo.description}</p>
         <BoxWrapper>
-          <AppInfoContainer>
-            <ImageWrapper isDriver={isDriver}>
-              <img src={appInfo.images.live} alt='' />
-            </ImageWrapper>
-            <h2>{appInfo.live_title}</h2>
-            {appInfo?.downloads && (
-              <DownLoadWrapper>
-                {appInfo.downloads.map(item => (
-                  <img
-                    src={item.icon}
-                    alt=''
-                    key={item.id}
-                    onClick={() => window.open(item.link, '_blank')}
-                  />
-                ))}
-              </DownLoadWrapper>
-            )}
-            {appInfo?.web_url && (
-              <DownloadLink href={appInfo.web_url} target='_blank' isSingle className='download-link'>
-                {appInfo.web_link_title}
-              </DownloadLink>
-            )}
-            <p>{appInfo.live_description}</p>
-            {appInfo?.purchase_message && (
-              <ButtonWRapper>
-                <Button
-                  color='primary'
-                  borderRadius='8px'
-                  onClick={() => window.open(appInfo?.purchase_link, '_blank')}
-                >
-                  {t('PURCHASE_NOW', 'Purchase Now')}
-                </Button>
-                {appInfo?.demo_book_message && (
-                  <Button
-                    color='primary'
-                    outline
-                    borderRadius='8px'
-                    onClick={() => window.open(appInfo?.demo_link, '_blank')}
-                  >
-                    {t('BOOK_A_DEMO', 'Book a demo')}
-                  </Button>
-                )}
-              </ButtonWRapper>
-            )}
-          </AppInfoContainer>
-          <AppInfoContainer>
-            <ImageWrapper isDriver={isDriver}>
-              <img src={appInfo.images.brand} alt='' />
-            </ImageWrapper>
-            <h2>{appInfo.brand_title}</h2>
-            <p>{appInfo.brand_description}</p>
-            <ButtonWRapper>
-              <Button
-                color='primary'
-                borderRadius='8px'
-                onClick={() => window.open(appInfo?.purchase_link, '_blank')}
-              >
-                {t('PURCHASE_NOW', 'Purchase Now')}
-              </Button>
-            </ButtonWRapper>
-          </AppInfoContainer>
+          {appInfo?.live_title && <LeftContent />}
+          <RightContent />
         </BoxWrapper>
         {children && children}
       </ContentWrapper>
--- src/components/MyProducts/KioskApp/index.js
+++ src/components/MyProducts/KioskApp/index.js
@@ -11,11 +11,8 @@ export const KioskApp = () => {
     title: t('ORDERING_SELF_KIOSK', 'Ordering Self Ordering Kiosk'),
     description: t('KIOS_DESCRIPTION', 'This tool will help your store complete that final step on each sale with innovative and intuitive software and hardware inside your locations.'),
     images: {
-      live: theme.images.myProducts.myKioskAppLive,
       brand: theme.images.myProducts.myKioskAppBrand
     },
-    live_title: t('LIVE_DEMO', 'Live Demo'),
-    live_description: t('KIOS_APP_LIVE_DEMO_DESCRIPTION', 'This is a branded Ordering.co product. play with it and understand how it works'),
     brand_title: t('YOUR_BRANDED_KIOSK', 'Your Branded Kiosk'),
     brand_description: t('KIOSK_BRANDED_DESCRIPTION', 'This App is delivered in less than five working days, fully branded with your guidelines, and ready to be used on any tablet you add it to.'),
     demo_book_message: 'I would like to book a Demo about the Kiosk app, can you assist me?',
@@ -23,7 +20,8 @@ export const KioskApp = () => {
     downloads: [
       { id: 1, icon: theme.images.myProducts.appStoreSmall, link: 'https://apps.apple.com/us/app/ordering-kiosk-2-0/id1615828153' },
       { id: 2, icon: theme.images.myProducts.playStoreSmall, link: 'https://play.google.com/store/apps/details?id=com.ordering.orderingkioskv5&hl=en&gl=US' }
-    ]
+    ],
+    demo_link: 'https://www.ordering.co/platform-demo-and-pricing'
   }
 
   return (
