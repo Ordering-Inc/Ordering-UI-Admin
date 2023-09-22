0b0284c1d23369d1ab3023025b9bad530c1bd436
dreamsky2020 <jintie@ordering.co>
2023-09-22 06:26:26 -0400

    [FIX] Correct contact layout

--- src/components/Orders/OrderContactInformation/index.js
+++ src/components/Orders/OrderContactInformation/index.js
@@ -106,17 +106,22 @@ export const OrderContactInformation = (props) => {
                 <InfoContent>
                   <div>
                     <p className='name'>{order?.business?.name}</p>
-                    {order?.business?.phone && (
-                      <IconButton
-                        onClick={() => window.open(`tel:${order.business.phone}`)}
-                      >
-                        <Telephone />
-                      </IconButton>
-                    )}
+                    <ActionIconWrapper>
+                      {order?.business?.phone && (
+                        <IconButton
+                          onClick={() => window.open(`tel:${order.business.phone}`)}
+                        >
+                          <Telephone />
+                        </IconButton>
+                      )}
+                    </ActionIconWrapper>
                   </div>
                   <p>{order?.business?.phone}</p>
                 </InfoContent>
-                <ChevronDown className='down-arrow' />
+                <ActionIconWrapper>
+                      <ChevronDown className='down-arrow' />
+                    </ActionIconWrapper>
+
               </BusinessInfo>
             </ContextAwareToggle>
             <Accordion.Collapse eventKey='0'>
