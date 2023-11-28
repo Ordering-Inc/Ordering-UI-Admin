d3b55e06ab566a1e8d5822788c76dff577abe0da
Jesus David <50583541+YisusJe@users.noreply.github.com>
2023-11-28 17:52:29 -0400

    [FIX] position of adjustment (#1221)
    Co-authored-by: YisusJe <jesus@ordering.co>

--- src/components/CartRecovery/OpenCartBill/index.js
+++ src/components/CartRecovery/OpenCartBill/index.js
@@ -175,7 +175,7 @@ export const OpenCartBill = (props) => {
               <td>
                 {t(extraValueAdjustment?.key?.toUpperCase(), extraValueAdjustment?.key)}{' '}
               </td>
-              <td>{parsePrice(parseFloat(extraValueAdjustment?.value))}</td>
+              <td>{parseFloat(extraValueAdjustment?.value) > 0 ? parsePrice(parseFloat(extraValueAdjustment?.value)) : `- ${parsePrice(parseFloat(extraValueAdjustment?.value) * -1)}`}</td>
             </tr>
           )}
         </tbody>
