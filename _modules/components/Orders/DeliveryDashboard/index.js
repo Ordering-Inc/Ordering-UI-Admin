1c0112d9df7488a386427a151da0bca2e72f8808
carlosg-ordering <carlos.g@ordering.co>
2024-01-03 16:01:29 -0600

    build

--- _modules/components/Orders/DeliveryDashboard/index.js
+++ _modules/components/Orders/DeliveryDashboard/index.js
@@ -44,7 +44,7 @@ var DeliveryDashboard = function DeliveryDashboard(props) {
     _useConfig2 = _slicedToArray(_useConfig, 1),
     configs = _useConfig2[0].configs;
   if (configs !== null && configs !== void 0 && configs.optimize_order_data && (configs === null || configs === void 0 || (_configs$optimize_ord = configs.optimize_order_data) === null || _configs$optimize_ord === void 0 ? void 0 : _configs$optimize_ord.value) === '1') {
-    ordersDashboardListProps.propsToFetch = ['app_id', 'business', 'business_id', 'created_at', 'customer', 'customer_id', 'delivery_type', 'driver_group_id', 'driver_id', 'driver', 'delivery_datetime', 'delivery_datetime_utc', 'external_id', 'eta_time', 'id', 'logistic_status', 'logistic_attemps', 'uuid', 'order_group', 'order_group_id', 'priority', 'summary', 'status', 'time_status', 'total'];
+    ordersDashboardListProps.propsToFetch = ['app_id', 'business', 'business_id', 'created_at', 'customer', 'customer_id', 'delivery_type', 'driver_group_id', 'driver_id', 'driver', 'delivery_datetime', 'delivery_datetime_utc', 'external_id', 'eta_time', 'id', 'logistic_status', 'logistic_attemps', 'uuid', 'order_group', 'order_group_id', 'priority', 'summary', 'status', 'time_status', 'total', 'metadata'];
   }
   var _useState = (0, _react.useState)(null),
     _useState2 = _slicedToArray(_useState, 2),
