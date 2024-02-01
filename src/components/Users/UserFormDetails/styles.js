3ae89ae056e4c9a1c11d54e4b5f20772227a42e2
carlosg-ordering <carlos.g@ordering.co>
2024-02-01 10:22:53 -0600

    [FEAT] add-timezone-selector-users

--- src/components/Users/UserFormDetails/styles.js
+++ src/components/Users/UserFormDetails/styles.js
@@ -171,3 +171,29 @@ export const WrapperUserTypeSelector = styled.div`
     color: ${props => props.theme.colors.secundaryContrast};
   }
 `
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 20px;
+
+  .select {
+    padding-top: 4px;
+    padding-bottom: 4px;
+    border: none;
+    background-color: ${props => props.theme.colors.secundary};
+    font-size: 14px;
+
+    ${({ isTimezone }) => isTimezone && css`
+      > div:first-child {
+        > div {
+          overflow: hidden;
+        }
+      }
+      .list-wrapper {
+        font-size: 12px;
+        max-height: 220px;
+      }
+    `}
+  }
+`
