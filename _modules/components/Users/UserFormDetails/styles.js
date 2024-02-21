62a0192c098ac7e2970344d93f5046a36466c524
Carlos Gomez <carlos.g@ordering.co>
2024-02-21 11:22:17 -0600

    build

--- _modules/components/Users/UserFormDetails/styles.js
+++ _modules/components/Users/UserFormDetails/styles.js
@@ -4,9 +4,9 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.WrapperUserTypeSelector = exports.WrapperPassword = exports.TogglePassword = exports.SkeletonForm = exports.FormInput = exports.DriverZoneRestrictionWrapper = exports.CalendarWrapper = exports.ActionsForm = void 0;
+exports.WrapperUserTypeSelector = exports.WrapperPassword = exports.TogglePassword = exports.SkeletonForm = exports.InputWrapper = exports.FormInput = exports.DriverZoneRestrictionWrapper = exports.CalendarWrapper = exports.ActionsForm = void 0;
 var _styledComponents = _interopRequireWildcard(require("styled-components"));
-var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5, _templateObject6, _templateObject7, _templateObject8, _templateObject9, _templateObject10, _templateObject11, _templateObject12, _templateObject13, _templateObject14;
+var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5, _templateObject6, _templateObject7, _templateObject8, _templateObject9, _templateObject10, _templateObject11, _templateObject12, _templateObject13, _templateObject14, _templateObject15, _templateObject16;
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
@@ -44,3 +44,10 @@ var WrapperUserTypeSelector = _styledComponents.default.div(_templateObject14 ||
   return props.theme.colors.secundaryContrast;
 });
 exports.WrapperUserTypeSelector = WrapperUserTypeSelector;
+var InputWrapper = _styledComponents.default.div(_templateObject15 || (_templateObject15 = _taggedTemplateLiteral(["\n  display: flex;\n  flex-direction: column;\n  width: 100%;\n  margin-bottom: 20px;\n\n  .select {\n    padding-top: 4px;\n    padding-bottom: 4px;\n    border: none;\n    background-color: ", ";\n    font-size: 14px;\n\n    ", "\n  }\n"])), function (props) {
+  return props.theme.colors.secundary;
+}, function (_ref2) {
+  var isTimezone = _ref2.isTimezone;
+  return isTimezone && (0, _styledComponents.css)(_templateObject16 || (_templateObject16 = _taggedTemplateLiteral(["\n      > div:first-child {\n        > div {\n          overflow: hidden;\n        }\n      }\n      .list-wrapper {\n        font-size: 12px;\n        max-height: 220px;\n      }\n    "])));
+});
+exports.InputWrapper = InputWrapper;
