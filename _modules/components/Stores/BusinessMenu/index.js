7946ecc7bd88cba15b3f2969a3cbacef0fe14742
Carlos Gomez <carlos.g@ordering.co>
2024-11-12 16:43:49 -0600

    build

--- _modules/components/Stores/BusinessMenu/index.js
+++ _modules/components/Stores/BusinessMenu/index.js
@@ -19,15 +19,15 @@ var _styles2 = require("./styles");
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
+function _toConsumableArray(r) { return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _iterableToArray(r) { if ("undefined" != typeof Symbol && null != r[Symbol.iterator] || null != r["@@iterator"]) return Array.from(r); }
+function _arrayWithoutHoles(r) { if (Array.isArray(r)) return _arrayLikeToArray(r); }
 function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
 function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
 function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-function _toConsumableArray(r) { return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread(); }
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-function _iterableToArray(r) { if ("undefined" != typeof Symbol && null != r[Symbol.iterator] || null != r["@@iterator"]) return Array.from(r); }
-function _arrayWithoutHoles(r) { if (Array.isArray(r)) return _arrayLikeToArray(r); }
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
@@ -62,10 +62,13 @@ var BusinessMenuUI = function BusinessMenuUI(props) {
     _useState4 = _slicedToArray(_useState3, 2),
     showOption = _useState4[0],
     setShowOption = _useState4[1];
-  var _useState5 = (0, _react.useState)(null),
+  var _useState5 = (0, _react.useState)({
+      menu: null,
+      loading: false
+    }),
     _useState6 = _slicedToArray(_useState5, 2),
-    currentMenu = _useState6[0],
-    setCurrentMenu = _useState6[1];
+    currentMenuState = _useState6[0],
+    setCurrentMenuState = _useState6[1];
   var _useState7 = (0, _react.useState)(false),
     _useState8 = _slicedToArray(_useState7, 2),
     isOpenSharedProduct = _useState8[0],
@@ -82,8 +85,13 @@ var BusinessMenuUI = function BusinessMenuUI(props) {
     _useState14 = _slicedToArray(_useState13, 2),
     initMenuId = _useState14[0],
     setInitMenuId = _useState14[1];
+  var setCurrentMenu = function setCurrentMenu(data) {
+    setCurrentMenuState(_objectSpread(_objectSpread({}, currentMenuState), data));
+  };
   var handleOpenOptions = function handleOpenOptions(name, menu) {
-    setCurrentMenu(menu);
+    setCurrentMenu({
+      menu: menu
+    });
     setIsExtendExtraOpen(true);
     setShowOption(name);
   };
@@ -91,7 +99,9 @@ var BusinessMenuUI = function BusinessMenuUI(props) {
     setShowOption(null);
     setIsExtendExtraOpen(false);
     setIsOpenSharedProduct(false);
-    setCurrentMenu(null);
+    setCurrentMenu({
+      menu: null
+    });
     if (!isTab) {
       setInitMenuId(null);
       (0, _utils.removeQueryToUrl)(['menu', 'product']);
@@ -182,10 +192,12 @@ var BusinessMenuUI = function BusinessMenuUI(props) {
       height: 24
     }));
   }), !(businessMenusState !== null && businessMenusState !== void 0 && businessMenusState.loading) && menuList.map(function (menu, index) {
+    var _currentMenuState$men;
     return /*#__PURE__*/_react.default.createElement(_styles2.MeunItem, {
+      disabled: currentMenuState === null || currentMenuState === void 0 ? void 0 : currentMenuState.loading,
       key: menu.id,
       isBorderTop: index === 0,
-      active: menu.id === (currentMenu === null || currentMenu === void 0 ? void 0 : currentMenu.id),
+      active: menu.id === (currentMenuState === null || currentMenuState === void 0 || (_currentMenuState$men = currentMenuState.menu) === null || _currentMenuState$men === void 0 ? void 0 : _currentMenuState$men.id),
       onClick: function onClick(e) {
         return handleOpenEdit(e, menu);
       }
@@ -205,7 +217,7 @@ var BusinessMenuUI = function BusinessMenuUI(props) {
   }, t('ADD_MENU', 'Add menu')))), width >= 1000 ? /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, showOption === 'option' && /*#__PURE__*/_react.default.createElement(_BusinessMenuOptions.BusinessMenuOptions, {
     open: showOption === 'option',
     business: business,
-    menu: currentMenu || {},
+    menu: (currentMenuState === null || currentMenuState === void 0 ? void 0 : currentMenuState.menu) || {},
     onClose: function onClose() {
       return handleCloseOption();
     },
@@ -227,7 +239,7 @@ var BusinessMenuUI = function BusinessMenuUI(props) {
   }, /*#__PURE__*/_react.default.createElement(_BusinessMenuOptions.BusinessMenuOptions, {
     open: showOption === 'option',
     business: business,
-    menu: currentMenu || {},
+    menu: (currentMenuState === null || currentMenuState === void 0 ? void 0 : currentMenuState.menu) || {},
     onClose: function onClose() {
       return handleCloseOption();
     },
