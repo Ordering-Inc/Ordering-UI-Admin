a51eac61debdfbe5be65c2241e5223ce9be2fb0a
Carlos Gomez <carlos.g@ordering.co>
2024-11-26 16:08:15 -0600

    build

--- _modules/components/Orders/AllInOne/index.js
+++ _modules/components/Orders/AllInOne/index.js
@@ -257,6 +257,7 @@ var OrdersManagerUI = function OrdersManagerUI(props) {
     setTimeStatus: setTimeStatus,
     setSlaSettingTime: setSlaSettingTime
   }), /*#__PURE__*/_react.default.createElement(_styles.MainContentContainer, null, /*#__PURE__*/_react.default.createElement(_styles.TopContent, null, /*#__PURE__*/_react.default.createElement(_styles.DriversContainer, null, /*#__PURE__*/_react.default.createElement(_DriversManager.DriversManager, {
+    enableSocketEventDriverRoom: true,
     disableSocketRoomDriver: true,
     disableDriverLocationsSockets: true,
     showCompressedInfo: true,
