b988c9b89aa46fee289129f7354a89540e86c4f3
Jesus David <50583541+YisusJe@users.noreply.github.com>
2025-07-08 14:16:46 -0400

    Agent businesses (#1422)
    * feat: add agents functionality to BusinessDetail and related components
    
    - Updated BusinessDetail to include 'agents' as a selectable tab and display BusinessOwners component for agents.
    - Modified BusinessInfoSettingList to add 'agents' to the settings list.
    - Enhanced BusinessOwnerSelector and BusinessOwners components to handle agents, including dynamic placeholder text and type-based functionality.
    - Updated UserDetails and UserDetailsMenu to accommodate call center agents with appropriate business fetching logic.
    
    * feat: add 'businesses_agent' field to ManagersList component
    
    - Updated ManagersList to include 'businesses_agent' in the user attributes list for enhanced data handling.

--- template/pages/ManagersList/index.js
+++ template/pages/ManagersList/index.js
@@ -16,7 +16,7 @@ export const ManagersList = (props) => {
       'country_phone_code', 'city_id', 'city', 'address', 'addresses',
       'address_notes', 'dropdown_option_id', 'dropdown_option', 'location',
       'zipcode', 'level', 'enabled', 'middle_name', 'second_lastname', 'birthdate', 'drivergroups',
-      'phone_verified', 'email_verified', 'wallets', 'push_tokens'
+      'phone_verified', 'email_verified', 'wallets', 'push_tokens', 'businesses_agent'
     ],
     onUserRedirect: (userId) => {
       if (!userId) {
